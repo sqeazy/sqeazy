df520aa8f157c6e6561f663a36e584045a5e13ea
Peter Steinbach <steinbach@scionics.de>
2014-11-03 17:24:32 +0100

    structure of sqy stands, implemented not very nicely (refactor later), need to add functionality

--- bench/sqy.cpp
+++ bench/sqy.cpp
@@ -14,39 +14,45 @@
 #include "bench_common.hpp"
 
 #include "boost/filesystem.hpp"
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
 
 struct sqy_config
 {
   bool verbose;
 };
 
-template <typename ModesContainer, typename ModesArgs>
+template <typename ModesContainer, typename ModesMap>
 int print_help(const ModesContainer& _av_pipelines ,
-	       const ModesArgs& _modes_args
+	       const ModesMap& _modes_args
 	       ) {
 
     std::string me = "sqy";
-    std::cout << "usage: " << me << " <-h|optional> <mode> <files|..>\n"
+    std::cout << "usage: " << me << " <-h|optional> <target> <files|..>\n"
               << "\n"
-              << "availble modes:\n"
-              << "\thelp" << "\n";
+              << "available targets:\n"
+              << "<help>" << "\n";
 
 
-    std::set<std::string> target_names;
+    
+    if(_modes_args.size()){
+      
+      for( auto args : _modes_args ){
+	
+	std::cout << "<" << args.first << ">\n" << args.second << "\n";
+      }
+    }
+
+    std::cout << "available pipelines:\n";
+    std::set<std::string> pipeline_names;
     for( auto it : _av_pipelines )
-        target_names.insert(it);
+        pipeline_names.insert(it);
 
-    for(const std::string& target : target_names ) {
-        std::cout << "\t" << target << "\n";
+    for(const std::string& pipeline : pipeline_names ) {
+        std::cout << "\t" << pipeline << "\n";
     }
 
     std::cout << "\n";
-    
-    if(_modes_args.size()){
-      for( auto args : _modes_args ){
-	std::cout << "\n";
-      }
-    }
 
     return 1;
 }
@@ -210,30 +216,76 @@ int main(int argc, char *argv[])
     decode_flow[modes[0]] = func_t(decompress_files<unsigned short, bswap1_lz4_pipe>);
     decode_flow[modes[1]] = func_t(decompress_files<unsigned short, rmbkg_bswap1_lz4_pipe>);
 
-    std::vector<std::string> args(argv+1, argv+argc);
+
+    static std::unordered_map<std::string,po::options_description> descriptions(2);
+
+      descriptions["compress"].add_options()
+      ("help", "produce help message")
+      ("verbose,v", "enable verbose output")
+      ("pipeline,p", po::value<std::string>()->default_value(modes[0]), "compression pipeline to be used")
+      ("files", po::value<std::vector<std::string> >()// ->composing()
+       , "")
+      ;
+
+      descriptions["decompress"].add_options()
+      ("help", "produce help message")
+      ("verbose,v", "enable verbose output")
+      ("files", po::value<std::vector<std::string> >()// ->composing()
+       , "")
+      ;
+
+
+    // std::vector<std::string> args(argv+1, argv+argc);
+    std::string joint_args;
+    for( int i = 0;i<argc;++i ){
+      joint_args.append(argv[i]);
+    }
     
     int retcode = 0;
-    if(argc<2 || args[0].find("-h")!=std::string::npos || args[0].find("help")!=std::string::npos) {
-      retcode = print_help(modes,args);
+    if(argc<2 || joint_args.find("-h")!=std::string::npos || joint_args.find("help")!=std::string::npos) {
+      retcode = print_help(modes,descriptions);
     }
     else{
-        unsigned int f_index = 1;
-
-        // std::unordered_map<std::string, func_t>::const_iterator f_func = prog_flow.end();
-
         
-        // fill_suite_config config(args);
 
-        // for(f_index = 0; f_index<args.size(); ++f_index) {
-        //     if((f_func = prog_flow.find(args[f_index])) != prog_flow.end())
-        //         break;
-        // }
+	po::options_description* desc = 0;
+	std::unordered_map<std::string, func_t> prog_flow;
 
-        // std::vector<std::string> filenames(args.begin()+1, args.end());
+	std::string target(argv[1]);
+	
+	if(descriptions.find(target)!=descriptions.end()){
+	  desc = &descriptions[target];
+	}
+	
+	//REFACTOR THIS!
+	if(target == "compress"){
+	  prog_flow = encode_flow;
+	}
 
-	std::cerr << "NOTHING IMPLEMENTED SO FAR\n";
-	//print_help(prog_flow);
-        
+	if(target == "decompress"){
+	  prog_flow = decode_flow;
+	}
+
+	if(prog_flow.empty() || !desc){
+	  std::cerr << "unable to decipher target: " << argv[1] << "\n";
+	  return print_help(modes,descriptions);
+	}
+
+	po::variables_map vm;
+	po::positional_options_description pd;
+	pd.add("files", -1);
+	
+	po::store(po::command_line_parser(argc-2, argv+2).options(*desc).positional(pd).allow_unregistered().run(), 
+		  vm); 
+
+	po::notify(vm);
+	
+	std::vector<std::string> inputFiles = vm["files"].as<std::vector<std::string> >();
+	
+	std::cout << target << " received files \n";
+	for( auto ffile : inputFiles ){
+	  std::cout << "\t"<< ffile << "\n";
+	}
     }
 
     return retcode;
