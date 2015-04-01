db8097e5e677b0a0ccfdf9bae9d2561d77dfcaef
Peter Steinbach <steinbac@mpi-cbg.de>
2015-04-01 15:28:07 +0200

    added preprocessor guards to check if __SSE*__ actually exist

--- src/sqeazy_common.hpp
+++ src/sqeazy_common.hpp
@@ -19,9 +19,23 @@ enum error_code {
   */
   namespace platform {
   
+#if defined __SSE2__ && defined __SSE2_MATH__
     struct has_sse2   {const static bool value = __SSE2__ && __SSE2_MATH__;};
+#else
+    struct has_sse2   {const static bool value = false;};
+#endif
+
+#if defined __SSE3__ && defined __SSSE3__
     struct has_sse3   {const static bool value = __SSE3__ && __SSSE3__;};
+#else
+    struct has_sse3   {const static bool value = false;};
+#endif
+
+#if defined __SSE4_2__ && defined __SSSE4_1__
     struct has_sse4   {const static bool value = __SSE4_2__ && __SSE4_1__;};
+#else
+    struct has_sse4   {const static bool value = false;};
+#endif
 
     struct use_vectorisation {
       const static bool value = (has_sse2::value && has_sse3::value && has_sse4::value) && _SQY_X_;
