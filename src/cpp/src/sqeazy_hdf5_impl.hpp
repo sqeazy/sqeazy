14b26cf00fbb68d91c3f0834f11982fa71a1b2d8
Peter Steinbach <steinbac@mpi-cbg.de>
2015-05-20 15:09:35 +0200

    replaced H5Z_filter_sqy with dummy version (seems not to be called in tests!)

--- src/cpp/src/sqeazy_hdf5_impl.hpp
+++ src/cpp/src/sqeazy_hdf5_impl.hpp
@@ -1,13 +1,23 @@
 #define _SQEAZY_HDF_CPP_
 
 #include "H5Cpp.h"
-//#include "hdf5.h"
 #include "sqeazy.h"
 #include "sqeazy_impl.hpp"
 #include "sqeazy_header.hpp"
 #include "sqeazy_predef_pipelines.hpp"
 
 size_t H5Z_filter_sqy(unsigned int _flags, //is it encode or decode
+			size_t _cd_nelmts,
+			const unsigned int _cd_values[], 
+			size_t _nbytes,
+			size_t *_buf_size, 
+			void **_buf
+			)
+{
+  return 0;
+}
+
+size_t H5Z_filter_sqy_impl(unsigned int _flags, //is it encode or decode
 			size_t _cd_nelmts,
 			const unsigned int _cd_values[], 
 			size_t _nbytes,
@@ -84,18 +94,6 @@ size_t H5Z_filter_sqy(unsigned int _flags, //is it encode or decode
   return ret;
   
 }
-
-// H5Z_SQY[1] = {
-//   H5Z_CLASS_T_VERS, /* H5Z_class_t version */
-//   H5Z_FILTER_SQY, /* Filter id number */
-//   1, /* encoder_present flag (set to true) */
-//   1, /* decoder_present flag (set to true) */
-//   "HDF5 sqy filter; see https://bitbucket.org/sqeazy/sqeazy",  /* Filter info */
-//   NULL, /* The "can apply" callback */
-//   NULL, /* The "set local" callback */
-//   (H5Z_func_t) H5Z_filter_sqy,  /* The filter function */
-// };
-
 
 H5PL_type_t   H5PLget_plugin_type() {return H5PL_TYPE_FILTER;}
 const void*   H5PLget_plugin_info() {return H5Z_SQY;}         
