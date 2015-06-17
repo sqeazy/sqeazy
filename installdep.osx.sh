e604a4a476bcbefbeef4f10fea525c14477d7f6d
Loic Alain Royer <royerloic@gmail.com>
2015-06-17 14:11:47 +0200

    small tweak

---
+++ installdep.osx.sh
@@ -0,0 +1,8 @@
+
+# Install dependencies with Brew on OSX:
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install hdf5
+brew install cmake
+brew install libtiff
+brew install lz4
+brew install boost
