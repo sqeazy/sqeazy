4d924d7cbecea68ccfcbb18c34a1394fbe3cf62c
Peter Steinbach <steinbac@mpi-cbg.de>
2016-02-26 17:41:56 +0100

    setup tests for newapi

--- src/cpp/tests/test_quantiser_impl.cpp
+++ src/cpp/tests/test_quantiser_impl.cpp
@@ -77,70 +77,6 @@ BOOST_AUTO_TEST_CASE( encodes ){
 }
 
 
-BOOST_AUTO_TEST_CASE( ramp_roundtrip ){
-
-  std::vector<uint16_t> input(1 << 12,0);
-  uint16_t value = 0;
-  for( uint16_t& _el : input )
-    _el = value++;
-  
-  std::vector<uint8_t> encoded(input.size(),0);
-
-  sqeazy::quantiser<uint16_t,uint8_t> shrinker(&input[0],&input[0]+input.size());
-  shrinker.encode(&input[0],input.size(),&encoded[0]);
-  
-  std::vector<uint16_t> reconstructed(input.size(),0);
-  shrinker.decode(&encoded[0],input.size(),&reconstructed[0]);
-
-  try{
-    BOOST_REQUIRE_EQUAL(reconstructed[0],input[0]);
-    BOOST_REQUIRE_EQUAL(reconstructed[1],input[0]);
-    BOOST_REQUIRE_EQUAL(reconstructed[15],input[0]);
-    BOOST_REQUIRE_NE(reconstructed[16],input[0]);
-    BOOST_REQUIRE_EQUAL(reconstructed[reconstructed.size()-1],input[reconstructed.size()-1-15]);
-  }
-  catch(...){
-    std::stringstream log_file;
-    log_file << "checkon_16bit_"
-	     << boost::unit_test::framework::current_test_case().p_name
-	     << ".log";
-    
-    shrinker.dump(log_file.str());
-    throw;
-  }
-  
-}
-
-BOOST_AUTO_TEST_CASE( capped_ramp_roundtrip ){
-
-  std::vector<uint16_t> input(1 << 12,0);
-  uint16_t value = 0;
-  for( uint16_t& _el : input )
-    _el = (value++) % 63;
-  
-  std::vector<uint8_t> encoded(input.size(),0);
-
-  sqeazy::quantiser<uint16_t,uint8_t> shrinker(&input[0],&input[0]+input.size());
-  shrinker.encode(&input[0],input.size(),&encoded[0]);
-  
-  std::vector<uint16_t> reconstructed(input.size(),0);
-  shrinker.decode(&encoded[0],input.size(),&reconstructed[0]);
-
-  try{
-    BOOST_REQUIRE_EQUAL_COLLECTIONS(input.begin(), input.end(),reconstructed.begin(), reconstructed.end());
-  }
-  catch(...){
-    std::stringstream log_file;
-    log_file << "checkon_16bit_"
-	     << boost::unit_test::framework::current_test_case().p_name
-	     << ".log";
-    
-    shrinker.dump(log_file.str());
-    throw;
-  }
-  
-}
-
 
 BOOST_AUTO_TEST_CASE( embryo_roundtrip ){
 
@@ -159,6 +95,23 @@ BOOST_AUTO_TEST_CASE( embryo_roundtrip ){
 				embryo_.data()+ embryo_.num_elements());
 }
 
+BOOST_AUTO_TEST_CASE( embryo_roundtrip_newapi ){
+
+  std::vector<uint8_t> encoded(embryo_.num_elements(),0);
+  
+  sqeazy::quantiser_scheme<uint16_t,uint8_t> shrinker;
+  shrinker.encode(embryo_.data(),&encoded[0],embryo_.num_elements());
+
+  std::vector<uint16_t> reconstructed(encoded.size(),0);
+  shrinker.decode(&encoded[0],
+		  &reconstructed[0],
+		  encoded.size());
+
+  BOOST_CHECK_EQUAL_COLLECTIONS(reconstructed.begin(), reconstructed.end(),embryo_.data(),
+				embryo_.data()+ embryo_.num_elements());
+}
+
+
 BOOST_AUTO_TEST_CASE( noisy_embryo_roundtrip ){
 
   std::vector<uint8_t> encoded(noisy_embryo_.num_elements(),0);
@@ -172,15 +125,28 @@ BOOST_AUTO_TEST_CASE( noisy_embryo_roundtrip ){
 		  encoded.size(),
 		  &reconstructed[0]);
 
-  std::vector<float> diff(noisy_embryo_.num_elements(),0);
-  std::transform(reconstructed.begin(), reconstructed.end(),noisy_embryo_.data(),diff.begin(),
-		 [](const uint16_t& _lhs,const uint16_t& _rhs){return float(float(_lhs) - float(_rhs));});
-
-  double l2norm = std::inner_product(diff.begin(), diff.end(),diff.begin(),0.);
+  double l2norm = sqeazy::l2norm(reconstructed.begin(), reconstructed.end(),noisy_embryo_.data());
 
   BOOST_REQUIRE_GT(l2norm,0);
-  l2norm = std::sqrt(l2norm)/noisy_embryo_.num_elements();
+  BOOST_CHECK_LT(l2norm,10);
 
+}
+
+BOOST_AUTO_TEST_CASE( noisy_embryo_roundtrip_newapi ){
+
+  std::vector<uint8_t> encoded(noisy_embryo_.num_elements(),0);
+
+  sqeazy::quantiser_scheme<uint16_t,uint8_t> shrinker;
+  shrinker.encode(noisy_embryo_.data(),&encoded[0],noisy_embryo_.num_elements());
+
+  std::vector<uint16_t> reconstructed(encoded.size(),0);
+  shrinker.decode(&encoded[0],
+		  &reconstructed[0],
+		  encoded.size());
+
+  double l2norm = sqeazy::l2norm(reconstructed.begin(), reconstructed.end(),noisy_embryo_.data());
+
+  BOOST_REQUIRE_GT(l2norm,0);
   BOOST_CHECK_LT(l2norm,10);
 
 }
@@ -511,7 +477,7 @@ BOOST_AUTO_TEST_CASE( write_to_file_realistic_roundtrip ){
   
 }
 
-BOOST_AUTO_TEST_CASE( write_to_file_realistic_roundtrip_newapi ){
+BOOST_AUTO_TEST_CASE( realistic_roundtrip_newapi ){
 
   std::stringstream lut_file;
   lut_file << "loics_suite_"
@@ -519,23 +485,25 @@ BOOST_AUTO_TEST_CASE( write_to_file_realistic_roundtrip_newapi ){
 	   << ".log";
     
   bfs::path tgt = lut_file.str(); 
-  sqeazy::quantiser<uint16_t,uint8_t> shrinker(&realistic_[0],
-					       &realistic_[0] + realistic_.size());
-
-  shrinker.lut_to_file(lut_file.str(),shrinker.lut_decode_);
-
-  BOOST_REQUIRE(bfs::exists(tgt));
-  BOOST_REQUIRE_NE(bfs::file_size(tgt),0u);
-
+  sqeazy::quantiser_scheme<uint16_t,uint8_t> shrinker;
   std::vector<uint8_t> encoded(realistic_.size(),0);
-  shrinker.encode(&realistic_[0],realistic_.size(),&encoded[0]);
+  auto end_ptr = shrinker.encode(&realistic_[0],&encoded[0],realistic_.size());
 
+  BOOST_CHECK(end_ptr!=nullptr);
+  BOOST_CHECK_EQUAL(end_ptr-&encoded[0],realistic_.size());
   
   std::vector<uint16_t> reconstructed(encoded.size(),0);
-  shrinker.decode(lut_file.str(),
-		  &encoded[0],
-		  encoded.size(),
-		  &reconstructed[0]);
+  int err =shrinker.decode(&encoded[0],
+			   &reconstructed[0],
+			   encoded.size());
+
+  BOOST_CHECK_EQUAL(err,0);
+  
+  // shrinker.lut_to_file(lut_file.str(),shrinker.lut_decode_);
+
+  // BOOST_REQUIRE(bfs::exists(tgt));
+  // BOOST_REQUIRE_NE(bfs::file_size(tgt),0u);
+
 
   size_t dyn_range = sqeazy::dyn_range(realistic_.begin(), realistic_.end());
   float exp_deviation_per_value = dyn_range / (256.f);
@@ -553,32 +521,33 @@ BOOST_AUTO_TEST_CASE( write_to_file_realistic_roundtrip_newapi ){
 	     << ".log";
     
     shrinker.dump(log_file.str());
+    BOOST_TEST_MESSAGE("wrote " << log_file.str());
   }
 
-  BOOST_TEST_MESSAGE("loics_suite/" << boost::unit_test::framework::current_test_case().p_name
-		     << "\t"  << "mse = " << mse << ", exp-mse = " << exp_mse);
+  // BOOST_TEST_MESSAGE("loics_suite/" << boost::unit_test::framework::current_test_case().p_name
+  // 		     << "\t"  << "mse = " << mse << ", exp-mse = " << exp_mse);
 
 
-  size_t reco_dyn_range = sqeazy::dyn_range(reconstructed.begin(), reconstructed.end());
-  BOOST_CHECK_LT(reco_dyn_range,dyn_range);
+  // size_t reco_dyn_range = sqeazy::dyn_range(reconstructed.begin(), reconstructed.end());
+  // BOOST_CHECK_LT(reco_dyn_range,dyn_range);
 
-  uint16_t min_val_original = *std::min_element(realistic_.begin(), realistic_.end());
-  uint16_t max_val_original = *std::max_element(realistic_.begin(), realistic_.end());
+  // uint16_t min_val_original = *std::min_element(realistic_.begin(), realistic_.end());
+  // uint16_t max_val_original = *std::max_element(realistic_.begin(), realistic_.end());
   
-  uint16_t min_val_reconstr = *std::min_element(reconstructed.begin(), reconstructed.end());
-  uint16_t max_val_reconstr = *std::max_element(reconstructed.begin(), reconstructed.end());
+  // uint16_t min_val_reconstr = *std::min_element(reconstructed.begin(), reconstructed.end());
+  // uint16_t max_val_reconstr = *std::max_element(reconstructed.begin(), reconstructed.end());
 
-  BOOST_CHECK_LE(max_val_reconstr,max_val_original);
-  BOOST_CHECK_GE(min_val_reconstr,min_val_original);
+  // BOOST_CHECK_LE(max_val_reconstr,max_val_original);
+  // BOOST_CHECK_GE(min_val_reconstr,min_val_original);
 
-  BOOST_TEST_MESSAGE("loics_suite/" << boost::unit_test::framework::current_test_case().p_name << " <original|reconstructed>"
-		     << "\t" << "min = " << min_val_original << " | " << min_val_reconstr
-		     << "\tmax = " << max_val_original << " | " << max_val_reconstr
-		     << "\tdrange = " << dyn_range << " | " << reco_dyn_range
-		     );
+  // BOOST_TEST_MESSAGE("loics_suite/" << boost::unit_test::framework::current_test_case().p_name << " <original|reconstructed>"
+  // 		     << "\t" << "min = " << min_val_original << " | " << min_val_reconstr
+  // 		     << "\tmax = " << max_val_original << " | " << max_val_reconstr
+  // 		     << "\tdrange = " << dyn_range << " | " << reco_dyn_range
+  // 		     );
 
-  if(bfs::exists(tgt))
-    bfs::remove(tgt);
+  // if(bfs::exists(tgt))
+  //   bfs::remove(tgt);
   
 }
 
@@ -635,33 +604,97 @@ BOOST_AUTO_TEST_CASE( normal_newapi ){
   std::vector<uint8_t> encoded(input.size(),0);
 
   sqeazy::quantiser_scheme<uint16_t,uint8_t> quant;
-
   uint8_t* end = quant.encode(&input[0],&encoded[0], input.size());
 
   BOOST_CHECK(end!=nullptr);
-  // float raw_sum = std::accumulate(input.begin(), input.end(),0);
-  // float sum = std::accumulate(encoded.begin(), encoded.end(),0);
-  // BOOST_CHECK_NE(sum,0);
-  // //TODO!!
-  // BOOST_CHECK_CLOSE_FRACTION(sum,raw_sum/16,1);
+  float raw_sum = std::accumulate(input.begin(), input.end(),0);
+  float sum = std::accumulate(encoded.begin(), encoded.end(),0);
+  BOOST_CHECK_NE(sum,0);
+  //TODO!!
+  BOOST_CHECK_CLOSE_FRACTION(sum,raw_sum/16,1);
 
-  // uint8_t max_value = *std::max_element(encoded.begin(), encoded.end());
-  // uint8_t max_expected = *std::max_element(input.begin(), input.end())/16;//16 is the bucketsize in this example
+  uint8_t max_value = *std::max_element(encoded.begin(), encoded.end());
+  uint8_t max_expected = *std::max_element(input.begin(), input.end())/16;//16 is the bucketsize in this example
 
-  // try{
-  //   BOOST_REQUIRE_CLOSE_FRACTION((float)max_value,(float)max_expected,5);
-  //   BOOST_REQUIRE_EQUAL(input[0],encoded[0]);
-  //   BOOST_REQUIRE_EQUAL(encoded[16],encoded[0]+1);
-  // }
-  // catch(...){
-  //   std::stringstream log_file;
-  //   log_file << "checkon_16bit_"
-  // 	     << boost::unit_test::framework::current_test_case().p_name
-  // 	     << ".log";
+  try{
+    BOOST_REQUIRE_CLOSE_FRACTION((float)max_value,(float)max_expected,5);
+    BOOST_REQUIRE_EQUAL(input[0],encoded[0]);
+    BOOST_REQUIRE_EQUAL(encoded[16],encoded[0]+1);
+  }
+  catch(...){
+    std::stringstream log_file;
+    log_file << "checkon_16bit_"
+  	     << boost::unit_test::framework::current_test_case().p_name
+  	     << ".log";
     
-  //   shrinker.dump(log_file.str());
-  //   throw;
-  // }
+    quant.dump(log_file.str());
+    throw;
+  }
+}
+
+
+BOOST_AUTO_TEST_CASE( ramp_roundtrip ){
+
+  std::vector<uint16_t> input(1 << 12,0);
+  uint16_t value = 0;
+  for( uint16_t& _el : input )
+    _el = value++;
+  
+  std::vector<uint8_t> encoded(input.size(),0);
+
+  sqeazy::quantiser<uint16_t,uint8_t> shrinker(&input[0],&input[0]+input.size());
+  shrinker.encode(&input[0],input.size(),&encoded[0]);
+  
+  std::vector<uint16_t> reconstructed(input.size(),0);
+  shrinker.decode(&encoded[0],input.size(),&reconstructed[0]);
+
+  try{
+    BOOST_REQUIRE_EQUAL(reconstructed[0],input[0]);
+    BOOST_REQUIRE_EQUAL(reconstructed[1],input[0]);
+    BOOST_REQUIRE_EQUAL(reconstructed[15],input[0]);
+    BOOST_REQUIRE_NE(reconstructed[16],input[0]);
+    BOOST_REQUIRE_EQUAL(reconstructed[reconstructed.size()-1],input[reconstructed.size()-1-15]);
+  }
+  catch(...){
+    std::stringstream log_file;
+    log_file << "checkon_16bit_"
+	     << boost::unit_test::framework::current_test_case().p_name
+	     << ".log";
+    
+    shrinker.dump(log_file.str());
+    throw;
+  }
+  
+}
+
+BOOST_AUTO_TEST_CASE( capped_ramp_roundtrip ){
+
+  std::vector<uint16_t> input(1 << 12,0);
+  uint16_t value = 0;
+  for( uint16_t& _el : input )
+    _el = (value++) % 63;
+  
+  std::vector<uint8_t> encoded(input.size(),0);
+
+  sqeazy::quantiser<uint16_t,uint8_t> shrinker(&input[0],&input[0]+input.size());
+  shrinker.encode(&input[0],input.size(),&encoded[0]);
+  
+  std::vector<uint16_t> reconstructed(input.size(),0);
+  shrinker.decode(&encoded[0],input.size(),&reconstructed[0]);
+
+  try{
+    BOOST_REQUIRE_EQUAL_COLLECTIONS(input.begin(), input.end(),reconstructed.begin(), reconstructed.end());
+  }
+  catch(...){
+    std::stringstream log_file;
+    log_file << "checkon_16bit_"
+	     << boost::unit_test::framework::current_test_case().p_name
+	     << ".log";
+    
+    shrinker.dump(log_file.str());
+    throw;
+  }
+  
 }
 
 BOOST_AUTO_TEST_SUITE_END()
