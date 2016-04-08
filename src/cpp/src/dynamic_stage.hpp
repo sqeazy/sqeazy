9d4fcc69832ed798cf9d60290866f082165eb5be
steinbac <steinbac@mpi-cbg.de>
2016-04-08 13:29:45 +0200

    fixed compile errors in stage chain due to obsolete  override final keywords

--- src/cpp/src/dynamic_stage.hpp
+++ src/cpp/src/dynamic_stage.hpp
@@ -186,109 +186,243 @@ namespace sqeazy{
     int decode(const std::int8_t* _in, raw_t* _out,std::vector<std::size_t> _shape) const override final { return 1; };
     
   };
-  /**
-     \brief method to create stage for a type given by a string
-     
-     \param[in] _name string to match type by
-     
-     \return 
-     \retval 
-     
-  */
-  template <typename pointee_t, typename Head>
-  std::shared_ptr<pointee_t> create_by_name(const std::string &_name,
-					    const std::string &_payload = "")
-  {
-    if(Head().name() == _name)
-    {
-      return std::dynamic_pointer_cast<pointee_t>(std::make_shared<Head>(_payload));
-    }
-    else
-    {
-      return nullptr;
-    }
-  }
-
-  template <typename pointee_t, typename Head, typename Second, typename... Tail>
-  std::shared_ptr<pointee_t> create_by_name(const std::string &_name,
-					    const std::string &_payload = "")
-  {
-    if(Head().name() == _name)
-    {
-      return std::dynamic_pointer_cast<pointee_t>(std::make_shared<Head>(_payload));
-    }
-    else
-    {
-      return create_by_name<pointee_t, Second, Tail...>(_name,_payload);
-    }
-  }
-
-  /**
-     \brief method to check if list of types contains type given by runtime string name
-     
-     \param[in] _name string to match type by
-     
-     \return 
-     \retval 
-     
-  */
-  template <class Head>
-  bool type_matches(const std::string &_name)
-  {
-    if(Head().name() == _name)
-    {
-      return true;
-    }
-    else
-    {
-      return false;
-    }
-  }
 
 
-  template <class Head, class Second, class... Tail>
-  bool type_matches(const std::string &_name)
-  {
-    if(Head().name() == _name)
-    {
-      // got it
-      return true;
-    }
-    else
-    {
-      return type_matches<Second, Tail...>(_name);
-    }
-  }
+  template <typename filter_t>
+  struct stage_chain {
 
-  /**
-     \brief factory object that will create a stage defined by a runtime string
-     
-     \param[in] _name runtime string to define the stage
-     
-     \return 
-     \retval 
-     
-  */
-  template <typename... available_types>
-  struct stage_factory
-  {
-    static_assert(sizeof...(available_types) > 0, "[dynamic_stage.hpp::stage_factory] Need at least one type for factory");
 
-    template <typename pointee_t>
-    static const std::shared_ptr<pointee_t> create(const std::string &_name,
-					    const std::string &_payload = "")
-    {
-      // static_assert(sizeof...(available_types) > 0, "Need at least one type for factory");
-      auto value = create_by_name<pointee_t,available_types...>(_name,_payload);
+    typedef typename filter_t::in_type incoming_t;
+    typedef typename filter_t::out_type outgoing_t;
+    
+    typedef std::shared_ptr< filter_t > filter_ptr_t;
+    typedef std::vector<filter_ptr_t>   filter_holder_t;
+
+    filter_holder_t chain_;
+
+    friend void swap(stage_chain & _lhs, stage_chain & _rhs){
+
+      std::swap(_lhs.chain_,_rhs.chain_);
+    }
+    
+    stage_chain():
+      chain_(){}
+
+    stage_chain(std::initializer_list<filter_ptr_t> _chain):
+      chain_(){
+
+      for(const filter_ptr_t& step : _chain)
+	{
+	  chain_.push_back(step);
+	}
+
+    }
+
+
+    stage_chain(const stage_chain& _rhs):
+      chain_(_rhs.chain_)
+    {}
+
+    stage_chain& operator=(stage_chain _rhs){
+
+      swap(*this, _rhs);
+
+      return *this;
+    }
+
+    const std::size_t size() const { return chain_.size(); }
+    const bool empty() const { return chain_.empty(); }
+
+    void push_back(const filter_ptr_t& _new){
+
+      chain_.push_back(_new);
       
-      return value;
     }
 
-    static const bool has(const std::string &_name) { return type_matches<available_types...>(_name); }
+    void add(const filter_ptr_t& _new){
+
+      this->push_back(_new);
+
+    }
+
+    bool is_compressor() const {
+      
+      return filter_t::is_compressor ;
+      
+    }
+    
+    bool valid_filters() const {
+
+      bool value = true;
+      if(chain_.empty())
+        return !value;
+
+      if(chain_.size()==1)
+	return true;//actually we cannot say anythin about validity here
+
+      for(unsigned i = 1; i < chain_.size(); ++i)
+	{
+	  auto this_filter =  const_stage_view(chain_[i]);
+	  auto last_filter =  const_stage_view(chain_[i-1]);
+
+	  if(!this_filter || !last_filter){
+	    value = false ;
+	    break;
+	  }
+	  value = value && (this_filter->input_type() == last_filter->output_type());
+	}
+
+      return value;
+      
+    }
+
+    filter_ptr_t& front() {
+
+      return chain_.front();
+      
+    }
+
+    const filter_ptr_t& front() const {
+
+      return chain_.front();
+      
+    }
+
+    filter_ptr_t& back() {
+
+      return chain_.back();
+      
+    }
+
+    const filter_ptr_t& back() const {
+
+      return chain_.back();
+      
+    }
+
+    std::string name() const
+    {
+
+      std::ostringstream value;
+
+      //TODO: REFACTOR THIS!
+      for(const auto& step : chain_)
+	{
+	  value << const_stage_view(step)->name();
+	  std::string cfg = const_stage_view(step)->config();
+
+	  if(!cfg.empty())
+	    value << "(" << cfg << ")";
+		
+	  if(step != chain_.back())
+	    value << "->";
+	}
+
+      return value.str();
+    }
+
+    std::string config() const
+    {
+      return name();
+    }
+
+    /**
+       \brief encode one-dimensional array _in and write results to _out
+       
+       \param[in] 
+       
+       \return 
+       \retval 
+       
+    */
+    
+    outgoing_t * encode(const incoming_t *_in, outgoing_t *_out, std::size_t _len) /*override final*/ {
+
+      std::vector<std::size_t> shape(1,_len);
+      return encode(_in,_out,shape);
+
+    }
+
+    
+    /**
+       \brief encode one-dimensional array _in and write results to _out
+       
+       \param[in] _in input buffer
+       \param[out] _out output buffer must be at least of size max_encoded_size
+       \param[in] _shape shape in input buffer in nDim
+
+       \return 
+       \retval 
+       
+    */
+    
+    outgoing_t* encode(const incoming_t *_in, outgoing_t *_out, std::vector<std::size_t> _shape) /*override final*/ {
+      outgoing_t* value = nullptr;
+      std::size_t len = std::accumulate(_shape.begin(), _shape.end(),1,std::multiplies<std::size_t>());
+      std::size_t max_len_byte = len*sizeof(incoming_t);
+
+      std::vector<incoming_t> temp_in(_in, _in+len);
+      std::vector<outgoing_t> temp_out(temp_in.size());
+
+      for( std::size_t fidx = 0;fidx<chain_.size();++fidx )
+	{
+	  
+	  auto encoded_end = chain_[fidx]->encode(&temp_in[0],
+						  &temp_out[0],
+						  _shape);
+	  value = reinterpret_cast<decltype(value)>(encoded_end);
+	  std::copy(&temp_out[0],value,temp_in.begin());
+	}
+
+      std::size_t outgoing_size = value-((decltype(value))&temp_out[0]);
+      std::copy(&temp_out[0],value, _out);
+           
+      return value;
+      
+    }
+
+    /**
+       \brief decode one-dimensional array _in and write results to _out
+       
+       \param[in] _in input buffer
+       \param[out] _out output buffer //NOTE: might be larger than _in for sink type pipelines
+       \param[in] _shape of input buffer size in units of its type, aka outgoing_t
+       
+       \return 
+       \retval 
+       
+    */
+    
+    int decode(const outgoing_t *_in, incoming_t *_out, std::vector<std::size_t> _shape) const /*override final*/ {
+      int value = 0;
+      int err_code = 0;
+      std::size_t len = std::accumulate(_shape.begin(), _shape.end(),1,std::multiplies<std::size_t>());
+      std::vector<incoming_t> temp(len,0);
+      std::copy(_in,_in+len,temp.begin());
+
+      for( std::size_t fidx = 0;fidx<chain_.size();++fidx )
+	{
+	    
+	  err_code = chain_[fidx]->decode(&temp[0],
+					  _out,
+					  _shape);
+	  value += err_code ? (10*(fidx+1))+err_code : 0;
+	  std::copy(_out, _out+len,temp.begin());
+	}
+
+      return value;
+
+    }
+
+    std::intmax_t max_encoded_size(std::intmax_t _incoming_size_byte){
+      if(!is_compressor())
+	return _incoming_size_byte;
+      else
+	return chain_.front()->max_encoded_size(_incoming_size_byte);
+    }
   };
 
-
-
+  
 };
 
 #endif /* _DYNAMIC_STAGE_H_ */
