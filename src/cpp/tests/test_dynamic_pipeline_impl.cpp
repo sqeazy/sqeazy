ca3df7458adcc1df1db1d860c761ecde9faec55e
steinbac <steinbac@mpi-cbg.de>
2016-04-12 14:30:32 +0200

    refactored stage_factory out into their own test

--- src/cpp/tests/test_dynamic_pipeline_impl.cpp
+++ src/cpp/tests/test_dynamic_pipeline_impl.cpp
@@ -6,59 +6,12 @@
 #include <vector>
 #include <cstdint>
 #include <sstream>
-#include "array_fixtures.hpp"
+//#include "array_fixtures.hpp"
 #include "encoders/sqeazy_impl.hpp"
 #include "dynamic_pipeline.hpp"
 #include "test_dynamic_pipeline_impl.hpp"
 
 namespace sqy = sqeazy;
-
-BOOST_AUTO_TEST_SUITE( factory_test_suite )
-
-
-  
-BOOST_AUTO_TEST_CASE( factory_finds_valid )
-{
-
-  auto local = int_factory_with_one_entry::create<int_filter>("add_one");
-  auto result = sqy::const_stage_view(local);
-  BOOST_CHECK_NE(result->name(),"");
-  BOOST_CHECK_EQUAL(result->name(),"add_one");
-    
-}
-
-BOOST_AUTO_TEST_CASE( factory_finds_valid_from_list )
-{
-
-  auto stage = int_factory::create<int_filter>("add_one");
-  // std::shared_ptr<int_stage> result = ;
-  auto result = sqy::const_stage_view(stage);
-  BOOST_CHECK_NE(result->name(),"");
-  BOOST_CHECK_EQUAL(result->name(),"add_one");
-  BOOST_CHECK(int_factory::has("square"));
-  
-}
-
-
-BOOST_AUTO_TEST_CASE( templated_factory_finds_valid )
-{
-
-  auto result = filter_factory<int>::create<int_filter>("square");
-  BOOST_REQUIRE(result!=nullptr);
-  BOOST_CHECK_NE(sqy::const_stage_view(result)->name(),"");
-  BOOST_CHECK_EQUAL(sqy::const_stage_view(result)->name(),"square");
-    
-}
-
-
-BOOST_AUTO_TEST_CASE( factory_finds_nothing )
-{
-
-  BOOST_CHECK(int_factory::create<int_filter>("dope")==nullptr);
-    
-}
-
-BOOST_AUTO_TEST_SUITE_END()
 
 
 BOOST_AUTO_TEST_SUITE( access_test_suite )
