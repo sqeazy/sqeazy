514f77aef2490c51c1ec602ab57fd443ec168f8e
Peter Steinbach <steinbac@mpi-cbg.de>
2016-04-22 13:32:28 +0200

    checking for OSX in bash script

--- let_me_build_that_for_you.sh
+++ let_me_build_that_for_you.sh
@@ -1,5 +1,13 @@
-if [[ -n $DYLD_LIBRARY_PATH ]];then
-    gradle build
+platform='unknown'
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+   platform='linux'
+else
+   platform='osx'
+fi
+
+if [[ $platform == 'osx' ]];then
+    gradle build -Phdf5_path=/usr/local/ -Plz4_path=/usr/local/ -Pboost_path=/usr/local/ -Pffmpeg_path=/usr/local/ -Ptiff_path=/usr/local
 else
     gradle build
 fi
