f92dea3cd963fd502311f59e0ad687971a36cbec
Peter Steinbach <steinbac@mpi-cbg.de>
2016-05-20 14:47:56 +0200

    added jest that mirrors java test

--- src/cpp/tests/test_pipeline_interface.cpp
+++ src/cpp/tests/test_pipeline_interface.cpp
@@ -116,3 +116,55 @@ BOOST_AUTO_TEST_CASE( roundtrip ){
   
 }
 BOOST_AUTO_TEST_SUITE_END()
+
+static const std::string tricky_filter_name = "quantiser->h264";
+
+BOOST_AUTO_TEST_SUITE( video_codecs )
+
+BOOST_AUTO_TEST_CASE( roundtrip ){
+
+  std::vector<size_t> shape(3,128);
+  shape.front() *= 2;
+  
+  const size_t len = std::accumulate(shape.begin(),
+				     shape.end(),
+				     1.,
+				     std::multiplies<size_t>());
+  
+  const unsigned long data_bytes = len*sizeof(std::uint16_t);
+  long length = data_bytes;
+
+  std::vector<std::uint16_t> inputdata(len,1);
+  std::size_t count=0;
+  for( std::uint16_t& n : inputdata )
+    n = (count++) % 65535;
+  
+  std::vector<std::uint16_t> outputdata(len,0);
+
+  
+  SQY_Pipeline_Max_Compressed_Length_3D_UI16(tricky_filter_name.c_str(),
+					     (long*)shape.data(),
+					     shape.size(),
+					     &length);
+  std::vector<char> compressed(length,0);
+  int rvalue = SQY_PipelineEncode_UI16(tricky_filter_name.c_str(),
+				       (const char*)inputdata.data(),
+				       (long*)shape.data(),
+				       shape.size(),
+				       compressed.data(),
+				       &length);
+  BOOST_REQUIRE_EQUAL(rvalue, 0);
+  BOOST_CHECK_LT(length,compressed.size());
+  
+  rvalue = SQY_PipelineDecode_UI16((const char*)compressed.data(),
+				   length,
+				   (char*)outputdata.data()
+			      );
+  
+  BOOST_REQUIRE_EQUAL(rvalue, 0);
+  BOOST_CHECK_EQUAL_COLLECTIONS(inputdata.begin(), inputdata.end(),
+				outputdata.begin(), outputdata.end()
+				);
+  
+}
+BOOST_AUTO_TEST_SUITE_END()
