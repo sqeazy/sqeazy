247473bc6984dc4051e8120aa01f9ecb6b91d10c
Peter Steinbach <steinbac@mpi-cbg.de>
2016-05-23 21:19:13 +0200

    added stepwise function as input signal to h264 tests, some ffmpeg/libavcodec parameters do not match similar operations on the command line

--- src/cpp/tests/test_h264_scheme_impl.cpp
+++ src/cpp/tests/test_h264_scheme_impl.cpp
@@ -69,32 +69,36 @@ BOOST_AUTO_TEST_CASE( lossy_roundtrip ){
   BOOST_CHECK_LT(bytes_written,embryo_.num_elements());
 
 
-  std::vector<std::uint8_t> retrieved(embryo_.num_elements(),0);
-  int err = scheme.decode(&encoded[0],
-			  &retrieved[0],
+  //std::vector<std::uint8_t> retrieved(embryo_.num_elements(),0);
+  sqy::uint8_image_stack roundtrip_ = embryo_;
+  int err = scheme.decode(encoded.data(),
+			  roundtrip_.data(),
 			  shape);
 
   BOOST_CHECK_EQUAL(err,0u);
 
-  sqy::uint8_image_stack_cref retrieved_cref(retrieved.data(),shape);
+  
+  sqy::uint8_image_stack_cref roundtrip_cref(roundtrip_.data(),shape);
   sqy::uint8_image_stack_cref embryo_cref(embryo_.data(),shape);
-  double l2norm = sqeazy::l2norm(retrieved_cref,embryo_cref);
-  double l1norm = sqeazy::l1norm(retrieved_cref,embryo_cref);
+  double l2norm = sqeazy::l2norm(roundtrip_cref,embryo_cref);
+  double l1norm = sqeazy::l1norm(roundtrip_cref,embryo_cref);
   
   BOOST_TEST_MESSAGE(boost::unit_test::framework::current_test_case().p_name << "\t l2norm = " << l2norm << ", l1norm = " << l1norm );
   
   try{
     BOOST_REQUIRE_LT(l2norm,1);    
-    BOOST_WARN_MESSAGE(std::equal(retrieved.begin(),
-    				     retrieved.begin()+retrieved.size(),
-    				     embryo_.data()),
+    BOOST_WARN_MESSAGE(std::equal(roundtrip_.data(),
+				  roundtrip_.data()+roundtrip_.num_elements(),
+				  embryo_.data()),
 		       "raw noisy volume and encoded/decoded volume do not match exactly, l2norm " << l2norm);
 
 	
   }
   catch(...){
     sqeazy::write_image_stack(embryo_,"embryo.tiff");
-    sqeazy::write_image_stack(retrieved_,"retrieved.tiff");
+    sqeazy::write_stack_as_y4m(embryo_,"embryo.y4m");
+    sqeazy::write_image_stack(roundtrip_,"retrieved.tiff");
+    sqeazy::write_stack_as_y4m(roundtrip_,"retrieved.y4m");
     throw;
   }
     
@@ -152,6 +156,74 @@ BOOST_AUTO_TEST_CASE( lossless_roundtrip ){
     
 }
 
+
+BOOST_AUTO_TEST_CASE( lossless_roundtrip_step_ramp ){
+
+  av_register_all();
+  
+  std::vector<std::uint8_t> encoded(embryo_.num_elements(),0);
+  std::vector<std::size_t> shape = {static_cast<uint32_t>(embryo_.shape()[sqy::row_major::z]),
+				    static_cast<uint32_t>(embryo_.shape()[sqy::row_major::y]),
+				    static_cast<uint32_t>(embryo_.shape()[sqy::row_major::x])};
+
+  sqeazy::h264_scheme<std::uint8_t> scheme("qp=0");
+
+  for(std::size_t i = 0;i<embryo_.num_elements();++i){
+    embryo_.data()[i] = 1 << (i % 8);
+    
+  }
+  
+  auto encoded_end = scheme.encode(embryo_.data(),
+				   &encoded[0],
+				   shape);
+  std::size_t bytes_written = encoded_end - &encoded[0];
+  encoded.resize(bytes_written);
+  
+  BOOST_CHECK(encoded_end!=nullptr);
+  BOOST_CHECK_NE(bytes_written,0u);
+  BOOST_CHECK_LT(bytes_written,embryo_.num_elements());
+
+
+  sqy::uint8_image_stack roundtrip = embryo_;
+  int err = scheme.decode(encoded.data(),
+			  roundtrip.data(),
+			  shape);
+
+  BOOST_CHECK_EQUAL(err,0u);
+
+  sqy::uint8_image_stack_cref roundtripcref(roundtrip.data(),shape);
+  sqy::uint8_image_stack_cref embryo_cref(embryo_.data(),shape);
+  double l2norm = sqeazy::l2norm(roundtripcref,embryo_cref);
+  double l1norm = sqeazy::l1norm(roundtripcref,embryo_cref);
+  
+  BOOST_TEST_MESSAGE(boost::unit_test::framework::current_test_case().p_name << "\t l2norm = " << l2norm << ", l1norm = " << l1norm );
+  
+  try{
+    BOOST_REQUIRE_LT(l2norm,1);    
+    BOOST_REQUIRE_MESSAGE(std::equal(roundtrip.data(),
+				     roundtrip.data()+roundtrip.num_elements(),
+				     embryo_.data()),
+			"raw noisy volume and encoded/decoded volume do not match exactly, l2norm " << l2norm);
+
+	
+  }
+  catch(...){
+    sqeazy::write_image_stack(embryo_,"embryo.tiff");
+    sqeazy::write_stack_as_y4m(embryo_,"embryo.y4m");
+    sqeazy::write_image_stack(roundtrip,"retrieved.tiff");
+    sqeazy::write_stack_as_y4m(roundtrip,"retrieved.y4m");
+    throw;
+  }
+  const size_t len = embryo_.num_elements();
+  
+  BOOST_REQUIRE_EQUAL_COLLECTIONS(embryo_.data(), embryo_.data()+10,
+				  roundtrip.data(), roundtrip.data()+10); 
+  BOOST_REQUIRE_EQUAL_COLLECTIONS(embryo_.data()+len-10, embryo_.data()+len,
+				  roundtrip.data()+len-10, roundtrip.data()+len);
+  BOOST_REQUIRE_EQUAL_COLLECTIONS(embryo_.data(), embryo_.data()+len,
+				  roundtrip.data(), roundtrip.data()+len);
+    
+}
 
 BOOST_AUTO_TEST_CASE( lossy_vs_lossless ){
     av_register_all();
