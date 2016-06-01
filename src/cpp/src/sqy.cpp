96270049a2b32a661862a68dac94b8308293861d
Peter Steinbach <steinbac@mpi-cbg.de>
2016-06-01 14:35:32 +0200

    added brief_help (continuing on to a hierarchichal parsing)

--- src/cpp/src/sqy.cpp
+++ src/cpp/src/sqy.cpp
@@ -68,8 +68,51 @@ void print_pairs(const std::vector<std::string>& _keys,
   }
 }
 
+
+template <typename options_type, typename map_type >
+int brief_help(const options_type& _sqy_options ,
+	       const map_type& _verb_descr, 
+	       const map_type& _verb_aliases 
+	       ) {
+
+  std::string me = "sqy";
+  std::cout << "usage: " << me << " <-h|optional> <verb> <files|..>\n"
+	    << "\n"
+	    << "available verbs (their description and aliases):\n"
+	    // << "\t" <<  "help" << "\n"
+    ;
+
+  std::vector<std::string> keys;
+  for(auto& pair : _verb_descr)
+    keys.push_back(pair.first);
+  
+  std::ostringstream alias;
+  for( auto& key  : keys  ){
+    
+    std::cout << std::setw(4) << " " << std::left << std::setw(15) << key ;
+    auto descr = _verb_descr.find(key);
+
+    if(descr!=_verb_descr.end())
+      std::cout << std::setw(4) << " " << std::left << std::setw(60) << descr->second ;
+    
+    if(_verb_aliases.find(key) != _verb_aliases.end()){
+      auto value = _verb_aliases.find(key);
+      std::cout << "(" << value->second << ")\n";
+    }
+    else
+      std::cout << "\n";
+  }
+  
+  std::cout << "\n"
+	    << "available flags to sqy only:\n"
+	    << _sqy_options << "\n";
+  
+  
+}
+
+
 template <typename modes_map_t, typename map_type >
-int print_help(
+int full_help(
 	       const modes_map_t& _modes_args,
 	       const map_type& _verb_aliases 
 	       ) {
@@ -130,13 +173,25 @@ int main(int argc, char *argv[])
 
 
   typedef std::function<void(const std::vector<std::string>&,const po::variables_map&) > func_t;
-    
-  // static std::vector<std::string> modes{sqeazy::bswap1_lz4_pipe::static_name(),
-  //     sqeazy::rmbkg_bswap1_lz4_pipe::static_name()};
-  const static std::string default_compression = "bitswap1->lz4";
 
+  po::options_description sqy_options;
+  sqy_options.add_options()
+    ("help,h", "produce this help message")
+    ("fullhelp", "produce exhaustive help message with all verbs documented")
+    ("version,v", "print version");
+
+  po::variables_map sqy_vm;
+  po::parsed_options sqy_parsed = po::command_line_parser(argc, argv
+						      ).options(sqy_options).allow_unregistered().run();
+  po::store(sqy_parsed, sqy_vm); 
+  po::notify(sqy_vm);
+
+  
+  
+  const static std::string default_compression = "bitswap1->lz4";
   static std::unordered_map<std::string,po::options_description> descriptions(4);
 
+  
   descriptions["compress"].add_options()
     ("help", "produce help message")
     ("verbose,v", "enable verbose output")
@@ -174,13 +229,22 @@ int main(int argc, char *argv[])
     ("metrics,m", po::value<std::string>()->default_value("nrmse"), "comma-separated list of metrics (possible values: mse, psnr)")
     ;
 
-  static     std::unordered_map<std::string, std::string> verb_aliases;
+  static     std::map<std::string, std::string> verb_aliases;
+  verb_aliases["help"] 	= std::string("");
   verb_aliases["compress"] 	= std::string("compress|enc|encode|comp");
   verb_aliases["decompress"] 	= std::string("decompress|dec|decode|rec");
   verb_aliases["scan"]		= std::string("scan|info");                
   verb_aliases["convert"] 	= std::string("convert|transform|trf");
   verb_aliases["compare"] 	= std::string("compare|cmp");
 
+  static     std::map<std::string, std::string> verb_descriptions;
+  verb_descriptions["help"] 	= std::string("print a help message");
+  verb_descriptions["compress"]	= std::string("compress a tiff stack to native sqy format or hdf5");
+  verb_descriptions["decompress"]= std::string("decompress a .sqy/.h5 file to tiff");
+  verb_descriptions["scan"]	= std::string("print statistics about the stack in a tiff file");
+  verb_descriptions["convert"] 	= std::string("convert a tiff file to another format (.yuv, .y4m)");
+  verb_descriptions["compare"] 	= std::string("compare two tiff stacks and see if they are equal");
+  
   static     std::unordered_map<std::string, func_t> verb_functors;
   verb_functors["compress"]	= compress_files;      
   verb_functors["decompress"]	= decompress_files;    
@@ -193,70 +257,82 @@ int main(int argc, char *argv[])
   for ( auto& pair : verb_aliases ){
     std::ostringstream rex;
     rex << "(" << pair.second << ")";
-    verb_aliases_rex[pair.first] = std::regex(rex.str());
+    if(!pair.second.empty())
+      verb_aliases_rex[pair.first] = std::regex(rex.str());
   }
     
-  // std::vector<std::string> args(argv+1, argv+argc);
   std::string joint_args;
   for( int i = 0;i<argc;++i ){
     joint_args.append(argv[i]);
   }
     
   int retcode = 0;
-  if(argc<2 || joint_args.find("-h")!=std::string::npos || joint_args.find("help")!=std::string::npos) {
-    retcode = print_help(descriptions,verb_aliases
+  if(argc<2 || // joint_args.find("-h")!=std::string::npos ||
+     joint_args.find("help")!=std::string::npos) {
+    retcode = brief_help(sqy_options,
+			 verb_descriptions,
+			 verb_aliases
 			 );
+    return retcode;
   }
-  else{
-        
-    po::options_description options_to_parse;
+
+  
+  po::options_description options_to_parse;
       
-    func_t prog_flow;
+  func_t prog_flow;
 
-    std::string target(argv[1]);
-    std::string verb;
-    
-    std::vector<char*> new_argv(argv,argv+argc);
-    new_argv.erase(new_argv.begin()+1);
+  std::string target(argv[1]);
+  std::string verb;
 
-
-    ///////////////////////////////////////////
-    //REFACTOR THIS!
-    for( auto& pair: verb_aliases_rex){
-      if(std::regex_match(target,pair.second))
-	{
-	  prog_flow = verb_functors[pair.first];
-	  verb = pair.first;
-	  break;
-	}
-    }
-
-    if(descriptions.find(verb)!=descriptions.end()){
-      options_to_parse.add(descriptions[verb]);
-    }
-    
-    po::variables_map vm;
-
-    po::parsed_options parsed = po::command_line_parser(new_argv.size(), &new_argv[0]// argc, argv
-							).options(options_to_parse).allow_unregistered().run();
-    po::store(parsed, vm); 
-    po::notify(vm);
-	
-    std::vector<std::string> inputFiles = po::collect_unrecognized(parsed.options, po::include_positional);
-
-    if(inputFiles.empty()){
-      std::cerr << "[sqy] no input files given, exiting ...\n";
-      return 1;
-    } else {
-      if(!verb.empty())
-	prog_flow(inputFiles, vm);
-      else{
-	std::cerr << "unable to find matching verb for " << target << "\n";
-	print_help(descriptions,verb_aliases);
-	return 1;
+  ///////////////////////////////////////////
+  //REFACTOR THIS!
+  for( auto& pair: verb_aliases_rex){
+    if(std::regex_match(target,pair.second))
+      {
+	prog_flow = verb_functors[pair.first];
+	verb = pair.first;
+	break;
       }
+  }
+
+  if(verb.empty()){
+    std::cerr << "unable to find matching verb for " << target << "\n";
+    full_help(descriptions,verb_aliases);
+    return 1;
+  }
+    
+  if(descriptions.find(verb)!=descriptions.end()){
+    options_to_parse.add(descriptions[verb]);
+  }
+    
+  po::variables_map vm;
+
+  std::vector<char*> argv_without_verb(argv,argv+argc);
+  argv_without_verb.erase(argv_without_verb.begin()+1);
+
+  po::parsed_options parsed = po::command_line_parser(argv_without_verb.size(), &argv_without_verb[0]// argc, argv
+						      ).options(options_to_parse).allow_unregistered().run();
+  po::store(parsed, vm); 
+  po::notify(vm);
+	
+  std::vector<std::string> inputFiles = po::collect_unrecognized(parsed.options, po::include_positional);
+
+  if(inputFiles.empty()){
+    std::cerr << "[sqy] no input files given, exiting ...\n";
+    return 1;
+  } else {
+    if(!verb.empty())
+      prog_flow(inputFiles, vm);
+    else{
+      std::cerr << "unable to find matching verb for " << target << "\n";
+      brief_help(sqy_options,
+			 verb_descriptions,
+			 verb_aliases
+			 );
+      return 1;
     }
   }
+  
 
   return retcode;
 }
