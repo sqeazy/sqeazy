a6abae00553504e1ed7f4938255142393fff1702
Peter Steinbach <steinbac@mpi-cbg.de>
2016-09-01 16:44:18 +0200

    moved entire post-process code and moved it into bundling

--- src/cpp/cmake/bundle_target.cmake
+++ src/cpp/cmake/bundle_target.cmake
@@ -27,15 +27,15 @@ endfunction(COPY_IN)
 # doc:
 function(COPY_IN_FORTARGET copydest _tgt files)
 
+
   set(flist ${ARGV})
   list(REMOVE_ITEM flist ${copydest} ${_tgt})
   
-  #message("++ [COPY_IN] received target dir ${copydest} and ${flist}")
   if(NOT TARGET bundle_copy_${_tgt})
     add_custom_target(bundle_copy_${_tgt})
   endif()
 
-  #TODO: append .so* files for UNIX
+
   if(UNIX)
     foreach(_ITEM IN LISTS flist)
 
@@ -80,6 +80,7 @@ function(BUNDLE tgt destdir)
   ## introspect target
   get_property(TGT_LIBS TARGET ${tgt} PROPERTY LINK_LIBRARIES)
   get_property(TGT_LDFLAGS TARGET ${tgt} PROPERTY LINK_FLAGS)
+  message("++ [BUNDLE] target yields LINK_FLAGS = ${TGT_LDFLAGS}")
   get_property(TGT_SRC TARGET ${tgt} PROPERTY SOURCES)
   get_property(TGT_HDR TARGET ${tgt} PROPERTY PUBLIC_HEADER)
   #get_property(TGT_ONAME TARGET ${tgt} PROPERTY OUTPUT_NAME)
@@ -110,12 +111,12 @@ function(BUNDLE tgt destdir)
     endif()
 
     get_filename_component(LIB_2_ADD_SUFFIX ${LIB_2_ADD} EXT)
-    if(${LIB_2_ADD_SUFFIX} STREQUAL ${CMAKE_STATIC_LIBRARY_SUFFIX})
-      #message("<< [BUNDLE ${_LIB}] NOT added to copy-in files (${LIB_2_ADD},${LIB_2_ADD_SUFFIX},${CMAKE_STATIC_LIBRARY_SUFFIX})")
-    else()
-      #message("<< [BUNDLE ${_LIB}] added to copy-in files with ${LIB_2_ADD}")
-      list(APPEND TGT_LIB_FILES ${LIB_2_ADD} )
-    endif()
+    # if(${LIB_2_ADD_SUFFIX} STREQUAL ${CMAKE_STATIC_LIBRARY_SUFFIX})
+    #message("<< [BUNDLE ${_LIB}] NOT added to copy-in files (${LIB_2_ADD},${LIB_2_ADD_SUFFIX},${CMAKE_STATIC_LIBRARY_SUFFIX})")
+    # else()
+    #message("<< [BUNDLE ${_LIB}] added to copy-in files with ${LIB_2_ADD}")
+    list(APPEND TGT_LIB_FILES ${LIB_2_ADD} )
+    #endif()
     unset(LIB_2_ADD)
   endforeach()
 
@@ -123,10 +124,10 @@ function(BUNDLE tgt destdir)
     COMMAND ${CMAKE_COMMAND} -E make_directory ${destdir})
   
   # cmake build system setup copy_in
-  message("++ [BUNDLE] copy-in for ${tgt}: ${TGT_LIB_FILES}")
-  copy_in_fortarget(${destdir} ${tgt} ${TGT_LIB_FILES})
+  #message("++ [BUNDLE] copy-in for ${tgt}: ${TGT_LIB_FILES}")
+  copy_in_fortarget(${destdir} ${tgt} "${TGT_LIB_FILES}")
   if(TGT_HDR)
-    copy_in_fortarget(${destdir} ${tgt} ${TGT_HDR})
+    copy_in_fortarget(${destdir} ${tgt} "${TGT_HDR}")
   endif()
 
   #TODO: try to build shared library from static external dependencies
@@ -144,6 +145,7 @@ function(BUNDLE tgt destdir)
   set_target_properties(bundle_${tgt} PROPERTIES INSTALL_RPATH ./)
   set_target_properties(bundle_${tgt} PROPERTIES BUILD_WITH_INSTALL_RPATH TRUE)
   set_target_properties(bundle_${tgt} PROPERTIES EXCLUDE_FROM_ALL TRUE)
+    
   set_target_properties(bundle_${tgt}
     PROPERTIES
     ARCHIVE_OUTPUT_DIRECTORY "${destdir}"
@@ -160,18 +162,125 @@ function(BUNDLE tgt destdir)
     if(WIN32)
       list(APPEND DEPS_FNAME_LIST ${destdir}\\${_FNAME})
     else()
-      if(NOT ${_FNAME} MATCHES ".*(lib|l)(m|dl|pthread)")#TODO: is this necessary, might only be required on Linux
-	list(APPEND DEPS_FNAME_LIST ${_PATH})
-      endif()
+      list(APPEND DEPS_FNAME_LIST ${_PATH})
     endif()
   endforeach()
+
+  ## treat ffmpeg dependencies in a special way
+  if(UNIX)
+    if( ${FFMPEG_USE_STATIC_LIBS})
+      if(UNIX AND NOT APPLE)
+	message(STATUS "[BUNDLE] adding LDFLAGS ${TGT_LDFLAGS}")
+	set_property(TARGET bundle_${tgt} APPEND PROPERTY LINK_FLAGS ${TGT_LDFLAGS})
+      endif()
+  
+      #TODO: remove this? as it might be handled by FFMPEG_EXTRA_LINK_FLAGS
+      #      this stuff should be resolved by the client
+      if(NOT DEFINED FORCE_STATIC_LIB_INCLUDE)
+
+	if(NOT SQY_MUST_INCLUDE_LIBS)
+	  set(SQY_MUST_INCLUDE_LIBS x264;x265)
+	endif()
+
+	message(STATUS "[Bundle] trying to resolve dependencies for ${SQY_MUST_INCLUDE_LIBS}")
+	
+	foreach(_DEP IN LISTS SQY_MUST_INCLUDE_LIBS)
+	  find_library(${_DEP}_STATIC_LIBRARY_PATH NAMES lib${_DEP}${CMAKE_STATIC_LIBRARY_SUFFIX})
+	  find_library(${_DEP}_DYNAMIC_LIBRARY_PATH NAMES ${_DEP} lib${_DEP} lib${_DEP}${CMAKE_SHARED_LIBRARY_SUFFIX})
+
+	  set(_DEP_INCLUDED FALSE)
+	  
+	  if(EXISTS ${${_DEP}_STATIC_LIBRARY_PATH} )
+
+	    get_filename_component(${_DEP}_RDIR ${${_DEP}_STATIC_LIBRARY_PATH} ABSOLUTE)
+	    get_filename_component(${_DEP}_RDIRFNAME ${${_DEP}_RDIR} DIRECTORY)
+
+	    link_directories(${${_DEP}_RDIRFNAME})
+	    if(NOT TARGET ${_DEP})
+	      add_library(${_DEP} STATIC IMPORTED)
+	      set_target_properties(${_DEP} PROPERTIES IMPORTED_LOCATION ${${_DEP}_RDIR})
+	      set_target_properties(${_DEP} PROPERTIES LINKER_LANGUAGE C)
+	      
+	      mark_as_advanced(${_DEP})
+	    endif()
+	    LIST (APPEND DEPS_FNAME_LIST ${_DEP})
+	    set(_DEP_INCLUDED TRUE)
+	    message("++ [Bundle] static ${_DEP} added ${${_DEP}_STATIC_LIBRARY_PATH}")
+	    
+	  else()
+	    
+	    if(EXISTS ${${_DEP}_DYNAMIC_LIBRARY_PATH})
+	      get_filename_component(${_DEP}_RDIR ${${_DEP}_DYNAMIC_LIBRARY_PATH} ABSOLUTE)
+	      get_filename_component(${_DEP}_RDIRFNAME ${${_DEP}_RDIR} DIRECTORY)
+
+	      if(NOT ${_DEP_INCLUDED})
+		link_directories(${${_DEP}_RDIRFNAME})
+		if(NOT TARGET ${_DEP})
+		  add_library(${_DEP} SHARED IMPORTED)
+		  set_target_properties(${_DEP} PROPERTIES IMPORTED_LOCATION ${${_DEP}_RDIR})
+		  set_target_properties(${_DEP} PROPERTIES LINKER_LANGUAGE C)
+		  mark_as_advanced(${_DEP})
+		endif()
+		LIST (APPEND DEPS_FNAME_LIST ${_DEP})
+		set(_DEP_INCLUDED TRUE)
+		message("++ [Bundle] shared ${_DEP} added ${${_DEP}_DYNAMIC_LIBRARY_PATH}")
+	      endif()
+	    else()
+	      message(WARNING "++ [BUNDLE] ${_DEP} not found on system")
+	    endif()
+	  endif()
+	  
+	  if(NOT ${_DEP_INCLUDED})
+	    message(WARNING "unable to locate lib${_DEP}, considered a dependency of requested FFMPEG targets")
+	  endif()
+
+	endforeach()
+	
+	# include(CheckFunctionExists)
+	# CHECK_FUNCTION_EXISTS(pow EXTRA_LIBM_NOT_NEEDED)
+	# if(NOT EXTRA_LIBM_NOT_NEEDED)
+	#   LIST (APPEND FFMPEG_LIBRARIES m)
+	# endif()
+      endif()
+
+      ## many/most Linux OS deploy their static libraries being compiled with -fPIE
+      ## it doesn't make too much sense to try and include them for the distribution of libsqeazy
+      ## the following tries to replace all static libraries with dynamic ones
+      if(NOT APPLE)
+	
+	if(NOT SQY_BLACKLIST_STATIC_LIBS)
+	  set(SQY_BLACKLIST_STATIC_LIBS "z;bz2;pthread;m;dl")
+	else()
+	  string(REPLACE " " ";" SQY_BLACKLIST_STATIC_LIBS "${}SQY_BLACKLIST_STATIC_LIBS")
+	endif()
+	
+	message("++ [BUNDLE] replacing blacklisted static libs ${SQY_BLACKLIST_STATIC_LIBS} in ${DEPS_FNAME_LIST}")
+	foreach(_BLACKLIB IN LISTS SQY_BLACKLIST_STATIC_LIBS)
+	  foreach(_DEP IN LISTS DEPS_FNAME_LIST)
+	    if(${_DEP} MATCHES "-l${_BLACKLIB}$" OR ${_DEP} MATCHES ".*lib${_BLACKLIB}${CMAKE_STATIC_LIBRARY_SUFFIX}")
+	      message("++ [BUNDLE] found ${_BLACKLIB} in ${_DEP}")
+	      list(REMOVE_ITEM DEPS_FNAME_LIST ${_DEP})
+	      list(FIND DEPS_FNAME_LIST ${CMAKE_SHARED_LIBRARY_PREFIX}${_BLACKLIB}${CMAKE_SHARED_LIBRARY_SUFFIX} _BLACKLIB_INDEX)
+	      if(${_BLACKLIB_INDEX} LESS 0)
+		list(APPEND DEPS_FNAME_LIST ${CMAKE_SHARED_LIBRARY_PREFIX}${_BLACKLIB}${CMAKE_SHARED_LIBRARY_SUFFIX})
+	      endif()
+	    else()
+	      continue()
+	    endif()
+	  endforeach()
+	endforeach()
+	
+      endif(NOT APPLE)
+
+    endif()
+  endif()
   
   message("++ [BUNDLE] link bundle to : ${DEPS_FNAME_LIST}")
   if(UNIX)
     if(APPLE)
       target_link_libraries(bundle_${tgt} ${DEPS_FNAME_LIST})
     else()
-      target_link_libraries(bundle_${tgt} ${DEPS_FNAME_LIST} pthread m dl)#TODO: is this necessary, might only be required on Linux
+      target_link_libraries(bundle_${tgt} ${DEPS_FNAME_LIST})#TODO: is this necessary, might only be required on Linux
     endif()
   else()
     target_link_libraries(bundle_${tgt} ${DEPS_FNAME_LIST})
