7f6e84908e190df9e7840d9115ef21f646677cf5
Peter Steinbach <steinbac@mpi-cbg.de>
2016-09-23 09:26:14 +0200

    started to refactor platform based implementations into their own headers

---
+++ src/cpp/src/compass_tags.hpp
@@ -0,0 +1,48 @@
+#ifndef _COMPASS_TAGS_H_
+#define _COMPASS_TAGS_H_
+
+#include "cpuid.h"
+#include <string>
+
+namespace compass {
+
+  namespace feature {
+
+    struct sse {};
+    struct sse2 {};
+    struct sse3 {};
+    struct sse4 {};
+
+    struct avx {};
+    struct avx2 {};
+
+  };
+
+  
+  namespace compiletime {
+
+    struct gnu_tag  {};
+    struct llvm_tag {};
+    struct msvc_tag {};
+
+    struct platform {
+
+#ifdef __GNUC__
+      typedef gnu_tag type;
+#endif
+
+#ifdef __clang__
+      typedef llvm_tag type;
+#endif
+      
+#ifdef __MSC_BUILD__
+      typedef msvc_tag type;
+#endif
+      
+    };
+  };
+
+  namespace ct = compiletime;
+  
+};
+#endif /* _COMPASS_TAGS_H_ */
