8986b0ba0c7556061bd75d00b810163e77b88e41
Peter Steinbach <steinbac@mpi-cbg.de>
2016-09-26 17:34:32 +0200

    implemented works and vendor function for msvc cpuid implementation

--- src/cpp/src/compass_msvc_impl.hpp
+++ src/cpp/src/compass_msvc_impl.hpp
@@ -2,7 +2,7 @@
 #define _COMPASS_MSVC_IMPL_H_
 
 #include <string>
-
+#include "intrin.h"
 #include "compass_tags.hpp"
 
 namespace compass {
@@ -12,16 +12,37 @@ namespace compass {
     namespace detail {
       
       static bool works(compiletime::msvc_tag) {
+		  std::int32_t regs[4] = { 0,0,0,0 };
+		  __cpuid(&regs[0],0);
 
-	return false;//not implemented yet
+		  if (regs[0] != 0)
+			  return true;
+		  else
+			  return false;
       
       }
 
-      static std::string vendor(compiletime::msvc_tag) {
+	  static std::string vendor(compiletime::msvc_tag) {
 
-	return "";//not implemented yet
-      
-      }
+		  std::int32_t regs[4] = { 0,0,0,0 };
+		  __cpuid(&regs[0], 0);
+
+		  std::string vendor_name;
+		  vendor_name.resize(3 * 4);
+
+		  std::copy(reinterpret_cast<char*>(&regs[1]), reinterpret_cast<char*>(&regs[1]) + 4,
+			  vendor_name.begin());
+		  std::copy(reinterpret_cast<char*>(&regs[2]), reinterpret_cast<char*>(&regs[2]) + 4,
+			  vendor_name.begin() + 4);
+		  std::copy(reinterpret_cast<char*>(&regs[3]), reinterpret_cast<char*>(&regs[3]) + 4,
+			  vendor_name.begin() + 8);
+
+		  if (regs[0] != 0)
+			  return vendor_name;
+		  else
+			  return "";
+		  
+	  }
 
       static bool has(feature::sse , ct::msvc_tag){
 	return false;
