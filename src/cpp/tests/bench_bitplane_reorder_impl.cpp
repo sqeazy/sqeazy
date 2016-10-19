5927ab1882ded06ffeb462013d37a20730b1aca7
Peter Steinbach <steinbac@mpi-cbg.de>
2016-10-19 16:32:06 +0200

    added small benchmark that compares runtime of scalar and sse bitplane reorder

---
+++ src/cpp/tests/bench_bitplane_reorder_impl.cpp
@@ -0,0 +1,73 @@
+#define BOOST_TEST_MODULE BENCH_BITPLANE_REORDER
+#include "boost/test/unit_test.hpp"
+
+#include <vector>
+#include <iostream>
+#include <algorithm> // for copy
+#include <iterator> // for ostream_iterator
+#include "encoders/sqeazy_impl.hpp"
+#include "sse_test_utils.hpp"
+
+
+
+typedef const_anyvalue_fixture<(1 << 15), 0xff> default_cv_fixture; 
+typedef ramp_fixture<(1 << 15)>			default_ramp_fixture; 
+
+
+
+BOOST_FIXTURE_TEST_SUITE( bench, default_cv_fixture )
+
+BOOST_AUTO_TEST_CASE( scalar_version ){
+
+
+  int ret = 0;
+
+  std::chrono::duration<double> time;
+  
+  for(int i = 0;i<20;++i){
+
+    auto start = std::chrono::high_resolution_clock::now();
+    ret = sqeazy::detail::scalar_bitplane_reorder_encode<1>(&input[0],
+							    &output[0],
+							    input.size());
+    auto end = std::chrono::high_resolution_clock::now();
+
+    BOOST_CHECK(ret==0);
+    std::fill(output.begin(), output.end(),0);
+
+    time += end-start;
+  }
+
+  BOOST_CHECK(time.count()>0);
+  std::cout << "scalar version: " << time.count()/20 << " s\n";
+
+}
+
+
+BOOST_AUTO_TEST_CASE( sse_version ){
+
+  int ret = 0;
+
+  std::chrono::duration<double> time;
+  
+  for(int i = 0;i<20;++i){
+
+    auto start = std::chrono::high_resolution_clock::now();
+    ret = sqeazy::detail::sse_bitplane_reorder_encode<1>(&input[0],
+							 &output[0],
+							 input.size());
+    auto end = std::chrono::high_resolution_clock::now();
+
+    BOOST_CHECK(ret==0);
+    std::fill(output.begin(), output.end(),0);
+
+    time += end-start;
+  }
+
+  BOOST_CHECK(time.count()>0);
+  std::cout << "sse version: " << time.count()/20 << " s\n";
+
+  
+}
+
+BOOST_AUTO_TEST_SUITE_END()
