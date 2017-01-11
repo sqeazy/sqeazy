0748b58ca30ef8a3c614db7573af75795f40cfc1
Peter Steinbach <steinbac@mpi-cbg.de>
2017-01-11 17:34:11 +0100

    fixed bug with wrong case

--- src/cpp/tests/test_pipeline_interface.cpp
+++ src/cpp/tests/test_pipeline_interface.cpp
@@ -24,7 +24,7 @@ BOOST_AUTO_TEST_SUITE( pipeline )
 BOOST_AUTO_TEST_CASE( does_this_validate ){
 
   bool answer = SQY_Pipeline_Possible(default_filter_name.c_str());
-  Boost_CHECK_EQUAL(answer, true);
+  BOOST_CHECK_EQUAL(answer, true);
 
 }
 
@@ -127,12 +127,12 @@ BOOST_AUTO_TEST_CASE( roundtrip ){
 
   std::vector<size_t> shape(3,128);
   shape.front() *= 2;
-  
+
   const size_t len = std::accumulate(shape.begin(),
 				     shape.end(),
 				     1.,
 				     std::multiplies<size_t>());
-  
+
   const unsigned long data_bytes = len*sizeof(std::uint16_t);
   long length = data_bytes;
 
@@ -140,10 +140,10 @@ BOOST_AUTO_TEST_CASE( roundtrip ){
   std::size_t count=0;
   for( std::uint16_t& n : inputdata )
     n = 1 << (count++ % 8);
-  
+
   std::vector<std::uint16_t> outputdata(len,0);
 
-  
+
   SQY_Pipeline_Max_Compressed_Length_3D_UI16(tricky_filter_name.c_str(),
 					     (long*)shape.data(),
 					     shape.size(),
@@ -157,39 +157,39 @@ BOOST_AUTO_TEST_CASE( roundtrip ){
 				       &length);
   BOOST_REQUIRE_EQUAL(rvalue, 0);
   BOOST_CHECK_LT(length,compressed.size());
-  
+
   rvalue = SQY_PipelineDecode_UI16((const char*)compressed.data(),
 				   length,
 				   (char*)outputdata.data()
 			      );
-  
+
   BOOST_REQUIRE_EQUAL(rvalue, 0);
   BOOST_REQUIRE_EQUAL_COLLECTIONS(inputdata.data(), inputdata.data()+10,
-				  outputdata.data(), outputdata.data()+10); 
+                  outputdata.data(), outputdata.data()+10);
   BOOST_REQUIRE_EQUAL_COLLECTIONS(inputdata.data()+len-10, inputdata.data()+len,
 				  outputdata.data()+len-10, outputdata.data()+len);
   BOOST_REQUIRE_EQUAL_COLLECTIONS(inputdata.data(), inputdata.data()+len,
 				  outputdata.data(), outputdata.data()+len);
-  
+
   // BOOST_CHECK_EQUAL_COLLECTIONS(inputdata.begin(), inputdata.end(),
   // 				outputdata.begin(), outputdata.end()
   // 				);
-  
+
 }
 
 
 BOOST_AUTO_TEST_CASE( quantiser_only ){
 
   const std::string filter_name = "quantiser";
-  
+
   std::vector<size_t> shape(3,128);
   shape.front() *= 2;
-  
+
   const size_t len = std::accumulate(shape.begin(),
 				     shape.end(),
 				     1.,
-				     std::multiplies<size_t>());
-  
+                     std::multiplies<size_t>());
+
   const unsigned long data_bytes = len*sizeof(std::uint16_t);
   long length = data_bytes;
 
@@ -198,10 +198,10 @@ BOOST_AUTO_TEST_CASE( quantiser_only ){
 
   for( std::uint16_t& n : inputdata )
     n = 1 << (count++ % 8);
-  
+
   std::vector<std::uint16_t> outputdata(len,0);
 
-  
+
   SQY_Pipeline_Max_Compressed_Length_3D_UI16(filter_name.c_str(),
 					     (long*)shape.data(),
 					     shape.size(),
@@ -215,23 +215,23 @@ BOOST_AUTO_TEST_CASE( quantiser_only ){
 				       &length);
   BOOST_REQUIRE_EQUAL(rvalue, 0);
   BOOST_CHECK_LT(length,compressed.size());
-  
+
   rvalue = SQY_PipelineDecode_UI16((const char*)compressed.data(),
 				   length,
 				   (char*)outputdata.data()
-			      );
-  
+                  );
+
   BOOST_REQUIRE_EQUAL(rvalue, 0);
   BOOST_REQUIRE_EQUAL_COLLECTIONS(inputdata.data(), inputdata.data()+10,
 				  outputdata.data(), outputdata.data()+10); 
   BOOST_REQUIRE_EQUAL_COLLECTIONS(inputdata.data()+len-10, inputdata.data()+len,
 				  outputdata.data()+len-10, outputdata.data()+len);
   BOOST_REQUIRE_EQUAL_COLLECTIONS(inputdata.data(), inputdata.data()+len,
-				  outputdata.data(), outputdata.data()+len);
-  
+                  outputdata.data(), outputdata.data()+len);
+
   // BOOST_CHECK_EQUAL_COLLECTIONS(inputdata.begin(), inputdata.end(),
   // 				outputdata.begin(), outputdata.end()
   // 				);
-  
+
 }
 BOOST_AUTO_TEST_SUITE_END()
