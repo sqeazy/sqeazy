a434d3f5b356a932df9af4d3284863cfb2cc94c2
Peter Steinbach <steinbac@mpi-cbg.de>
2017-01-11 17:33:20 +0100

    commented out legacy code

--- src/cpp/src/string_parsers.hpp
+++ src/cpp/src/string_parsers.hpp
@@ -14,9 +14,9 @@
 
 namespace sqeazy {
 
-  namespace qi = boost::spirit::qi;
-  namespace ascii = boost::spirit::ascii;
-  namespace phoenix = boost::phoenix;
+  // namespace qi = boost::spirit::qi;
+  // namespace ascii = boost::spirit::ascii;
+  // namespace phoenix = boost::phoenix;
 
   typedef std::vector<std::string> vec_of_strings_t;
   typedef std::vector<boost::string_ref> vec_of_string_refs_t;
@@ -28,38 +28,38 @@ namespace sqeazy {
   static const std::pair<boost::string_ref, boost::string_ref> ignore_this_delimiters_ref = std::make_pair(boost::string_ref(ignore_this_delimiters.first),
                                                                                                            boost::string_ref(ignore_this_delimiters.second));
 
-  template <typename char_itr_t>
-  vec_of_strings_t split_by(char_itr_t _begin, char_itr_t _end,
-                            const std::string& _sep = ","
-                            ){
+  // template <typename char_itr_t>
+  // vec_of_strings_t split_by(char_itr_t _begin, char_itr_t _end,
+  //                           const std::string& _sep = ","
+  //                           ){
 
-    std::string allowed_characters = "a-zA-Z_0-9.=";
-    auto f_itr = allowed_characters.find(_sep);
+  //   std::string allowed_characters = "a-zA-Z_0-9.=";
+  //   auto f_itr = allowed_characters.find(_sep);
 
-    if(f_itr != std::string::npos)
-      allowed_characters.erase(f_itr,f_itr + _sep.size());
+  //   if(f_itr != std::string::npos)
+  //     allowed_characters.erase(f_itr,f_itr + _sep.size());
 
-    vec_of_strings_t v;
+  //   vec_of_strings_t v;
 
-    bool r = qi::parse( _begin,                          /*< start iterator >*/
-                        _end,                           /*< end iterator >*/
-                        (
-                         +qi::char_(allowed_characters) % +qi::lit(_sep)
-                         ),
-                        v
-                        );
+  //   bool r = qi::parse( _begin,                          /*< start iterator >*/
+  //                       _end,                           /*< end iterator >*/
+  //                       (
+  //                        +qi::char_(allowed_characters) % +qi::lit(_sep)
+  //                        ),
+  //                       v
+  //                       );
 
-    if (_begin != _end  && !r) // fail if we did not get a full match
-      {
-        std::string msg(_begin,_end);
-        std::cerr << "[sqeazy::split_by] couldn't split anything in " << msg << "\n";
-        v.clear();
-        return v;
-      }
+  //   if (_begin != _end  && !r) // fail if we did not get a full match
+  //     {
+  //       std::string msg(_begin,_end);
+  //       std::cerr << "[sqeazy::split_by] couldn't split anything in " << msg << "\n";
+  //       v.clear();
+  //       return v;
+  //     }
 
-    return v;
+  //   return v;
 
-  }
+  // }
 
 
   template <typename string_t>
@@ -159,88 +159,83 @@ namespace sqeazy {
     boost::string_ref data_ref = _data;
 
     value = split_string_ref_by(data_ref, _sep);
-    
+
     return value;
 
   }
 
-  
 
-  
-  
-  template <typename Iterator>
-    struct ordered_command_sequence 
-      : qi::grammar<Iterator, vec_of_pairs_t()>
-    {
-      ordered_command_sequence()
-          : ordered_command_sequence::base_type(query)
-        {
-	  query =  pair >> *(qi::lit("->") >> pair);
-	  pair  =  key >> -('(' >> value >> ')');
-	  key   =  qi::char_("a-zA-Z") >> *qi::char_("a-zA-Z_0-9");
-	  //value =  +qi::char_("a-zA-Z_0-9:=,\"/");
-	  value =  +(qi::print - qi::char_("()"));
-        }
+  // template <typename Iterator>
+  //   struct ordered_command_sequence
+  //     : qi::grammar<Iterator, vec_of_pairs_t()>
+  //   {
+  //     ordered_command_sequence()
+  //         : ordered_command_sequence::base_type(query)
+  //       {
+  //     query =  pair >> *(qi::lit("->") >> pair);
+  //     pair  =  key >> -('(' >> value >> ')');
+  //     key   =  qi::char_("a-zA-Z") >> *qi::char_("a-zA-Z_0-9");
+  //     //value =  +qi::char_("a-zA-Z_0-9:=,\"/");
+  //     value =  +(qi::print - qi::char_("()"));
+  //       }
 
-        qi::rule<Iterator, vec_of_pairs_t()> query;
-        qi::rule<Iterator, std::pair<std::string, std::string>()> pair;
-        qi::rule<Iterator, std::string()> key, value;
-    };
-  
-  template <typename char_itr_t>
-  vec_of_pairs_t parse_by(char_itr_t _begin, char_itr_t _end, const std::string& _sep = ","){
+  //       qi::rule<Iterator, vec_of_pairs_t()> query;
+  //       qi::rule<Iterator, std::pair<std::string, std::string>()> pair;
+  //       qi::rule<Iterator, std::string()> key, value;
+  //   };
 
-    // ordered_command_sequence<std::string::iterator> p;
-    ordered_command_sequence<char_itr_t> p;
-    vec_of_pairs_t v;
+  // template <typename char_itr_t>
+  // vec_of_pairs_t parse_by(char_itr_t _begin, char_itr_t _end, const std::string& _sep = ","){
 
-    bool r = qi::parse(_begin, _end, p, v);
-    if (_begin != _end  && !r) // fail if we did not get a full match
-      {
-	std::string msg(_begin,_end);
-	std::cerr << "[sqeazy::parse_by] failed to parse " << msg << "\n";
-	v.clear();
-	return v;
-      }
+  //   // ordered_command_sequence<std::string::iterator> p;
+  //   ordered_command_sequence<char_itr_t> p;
+  //   vec_of_pairs_t v;
 
-    return v;
-  }
-  
-  template <typename char_itr_t>
-  parsed_map_t unordered_parse_by(char_itr_t _begin, char_itr_t _end,
-				  const std::string& _pair_sep = ",",
-				  const std::string& _kv_sep = "="){
+  //   bool r = qi::parse(_begin, _end, p, v);
+  //   if (_begin != _end  && !r) // fail if we did not get a full match
+  //     {
+  //   std::string msg(_begin,_end);
+  //   std::cerr << "[sqeazy::parse_by] failed to parse " << msg << "\n";
+  //   v.clear();
+  //   return v;
+  //     }
 
-    using return_t = std::map<std::string,std::string>;
+  //   return v;
+  // }
 
-    return_t value;
+  // template <typename char_itr_t>
+  // parsed_map_t unordered_parse_by(char_itr_t _begin, char_itr_t _end,
+  //   			  const std::string& _pair_sep = ",",
+  //   			  const std::string& _kv_sep = "="){
 
-    vec_of_strings_t parts = split_by(_begin,_end,_pair_sep);
-    vec_of_strings_t key_value;
+  //   using return_t = std::map<std::string,std::string>;
+
+  //   return_t value;
+
+  //   vec_of_strings_t parts = split_by(_begin,_end,_pair_sep);
+  //   vec_of_strings_t key_value;
     
-    for( auto & str : parts ){
+  //   for( auto & str : parts ){
 
-      key_value = split_by(str.begin(),
-			   str.end(),
-			   _kv_sep);
+  //     key_value = split_by(str.begin(),
+  //   		   str.end(),
+  //   		   _kv_sep);
 
-      if(key_value.size()==1)
-	value[key_value.front()] = "";
+  //     if(key_value.size()==1)
+  //   value[key_value.front()] = "";
       
-      if(key_value.size()>1 && key_value.size() % 2 == 0)
-	value[key_value.front()] = key_value.back();
+  //     if(key_value.size()>1 && key_value.size() % 2 == 0)
+  //   value[key_value.front()] = key_value.back();
 
-      key_value.clear();
+  //     key_value.clear();
 
-    }
-    
-    return value;
-    
-  }
+  //   }
+  //   return value;
+  // }
 
   parsed_map_t parse_string_by(const std::string& _payload,
-			       const std::string& _pair_sep = ",",
-			       const std::string& _kv_sep = "="){
+                               const std::string& _pair_sep = ",",
+                               const std::string& _kv_sep = "="){
 
     using return_t = std::map<std::string,std::string>;
 
@@ -248,24 +243,24 @@ namespace sqeazy {
 
     vec_of_strings_t parts = split_string_by(_payload,_pair_sep);
     vec_of_strings_t key_value;
-    
+
     for( auto & str : parts ){
 
       key_value = split_string_by(str,
-				  _kv_sep);
+                                  _kv_sep);
 
       if(key_value.size()==1)
-	value[key_value.front()] = "";
-      
+        value[key_value.front()] = "";
+
       if(key_value.size()>1 && key_value.size() % 2 == 0)
-	value[key_value.front()] = key_value.back();
+        value[key_value.front()] = key_value.back();
 
       key_value.clear();
 
     }
-    
+
     return value;
-    
+
   }
 
 
