2897efc2fb2db736136fe61fce585a817cb99f4b
Peter Steinbach <steinbac@mpi-cbg.de>
2017-01-16 15:42:27 +0100

    added missing header

--- src/cpp/src/detail/compass_tags.hpp
+++ src/cpp/src/detail/compass_tags.hpp
@@ -2,12 +2,13 @@
 #define _COMPASS_TAGS_H_
 
 #include <string>
+#include <climits>
 
 namespace compass {
 
-  
+
   namespace feature {
-    
+
     //SIMD instruction sets
     //128bit wide
     struct sse {};
@@ -19,12 +20,12 @@ namespace compass {
     struct avx {};
     struct avx2 {};
 
-    
+
   };
 
-  
+
   namespace compiletime {
-    
+
     struct gnu_tag  {};
     struct llvm_tag {};
     struct msvc_tag {};
@@ -39,40 +40,40 @@ namespace compass {
 #endif
 #endif
 
-      
+
 #ifdef _MSC_BUILD
       typedef msvc_tag type;
 #endif
-      
+
     };
 
     //architectures
     struct x86_tag {};
     struct arm_tag {};
-    
-    
+
+
     struct platform {
 
 #if __x86_64 || __x86_64__ || __i386 || i386 || __i386__ || _M_IX86 == 600 | _M_X64 == 100
       typedef x86_tag type;
 #endif
-      
+
       //32 or 64bit?
       const static int bitness = sizeof(void*)*CHAR_BIT;
-      
+
     };
 
-    
+
     //cpuid register locations
     static const int eax = 0;
     static const int ebx = 1;
     static const int ecx = 2;
     static const int edx = 3;
 
-    
+
   };
 
   namespace ct = compiletime;
-  
+
 };
 #endif /* _COMPASS_TAGS_H_ */
