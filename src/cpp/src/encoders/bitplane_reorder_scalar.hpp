0ee8d3e8e05cb132aac003c4ca1227de9ce92ffc
Peter Steinbach <steinbac@mpi-cbg.de>
2017-01-16 15:51:39 +0100

    added openmp to bitswap scalar

--- src/cpp/src/encoders/bitplane_reorder_scalar.hpp
+++ src/cpp/src/encoders/bitplane_reorder_scalar.hpp
@@ -7,50 +7,62 @@
 #include "sqeazy_common.hpp"
 #include "scalar_utils.hpp"
 
+#ifdef _OPENMP
+#include "omp.h"
+#endif
 
 namespace sqeazy {
 
   namespace detail {
 
-    
+
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     //scalar implementation
     template <const unsigned num_bits_per_plane, 
-	      typename raw_type , 
-	      typename size_type
-	      >
+              typename raw_type ,
+              typename size_type
+              >
     static const error_code scalar_bitplane_reorder_encode(const raw_type* _input,
-							   raw_type* _output,
-							   const size_type& _length){
+                                                           raw_type* _output,
+                                                           const size_type& _length,
+                                                           int num_threads = 1)
+    {
+
+
 
       static const unsigned type_width = CHAR_BIT*sizeof(raw_type);
       static const unsigned num_planes = type_width/num_bits_per_plane;
 
       const unsigned segment_length = _length/num_planes;
-      
+
       const raw_type mask = ~(~0 << (num_bits_per_plane));//mask to extract all last bits up to bit at num_bits_per_plane
       raw_type value = 0;
 
+
+#pragma omp parallel for \
+  private(value) \
+  shared(_input, _output) \
+  num_threads(num_threads)
       for(size_type index = 0; index < _length; ++index) {
-	  
 
-	value = xor_if_signed(_input[index]);
-	value = rotate_left<1>(value);
 
-	size_type output_bit_offset = (type_width - num_bits_per_plane) - ((index % num_planes)*num_bits_per_plane);
+        value = xor_if_signed(_input[index]);
+        value = rotate_left<1>(value);
 
-	for(unsigned plane_index = 0; plane_index<num_planes; ++plane_index) {
+        size_type output_bit_offset = (type_width - num_bits_per_plane) - ((index % num_planes)*num_bits_per_plane);
 
-	  size_type input_bit_offset = plane_index*num_bits_per_plane;
-	  raw_type extracted_bits = (value >> input_bit_offset) & mask;
+        for(unsigned plane_index = 0; plane_index<num_planes; ++plane_index) {
 
-	  size_type output_index = ((num_planes-1-plane_index)*segment_length) + (index/num_planes);
+          size_type input_bit_offset = plane_index*num_bits_per_plane;
+          raw_type extracted_bits = (value >> input_bit_offset) & mask;
 
-	  _output[output_index] = setbits_of_integertype(_output[output_index],
-							 extracted_bits,
-							 output_bit_offset,
-							 num_bits_per_plane);
-	}
+          size_type output_index = ((num_planes-1-plane_index)*segment_length) + (index/num_planes);
+
+          _output[output_index] = setbits_of_integertype(_output[output_index],
+                                                         extracted_bits,
+                                                         output_bit_offset,
+                                                         num_bits_per_plane);
+        }
       }
 
       return SUCCESS;
@@ -58,13 +70,14 @@ namespace sqeazy {
     }
 
 
-    template <const unsigned num_bits_per_plane, 
-	      typename raw_type , 
-	      typename size_type
-	      >
+    template <const unsigned num_bits_per_plane,
+              typename raw_type ,
+              typename size_type
+              >
     static const error_code scalar_bitplane_reorder_decode(const raw_type* _input,
-							   raw_type* _output,
-							   const size_type& _length){
+                                                           raw_type* _output,
+                                                           const size_type& _length,
+                                                           int num_threads = 1){
 
       static const unsigned raw_type_num_bits = sizeof(raw_type)*CHAR_BIT;
       static const unsigned num_planes = raw_type_num_bits/num_bits_per_plane;
@@ -73,30 +86,33 @@ namespace sqeazy {
       const raw_type mask = ~(~0 << (num_bits_per_plane));
       static const unsigned type_width = CHAR_BIT*sizeof(raw_type);
 
+#pragma omp parallel for shared(_input, _output)    \
+  num_threads(num_threads)
       for(size_type plane_index = 0; plane_index<num_planes; ++plane_index) {
 
+        size_type output_bit_offset = (plane_index*num_bits_per_plane);
 
-	size_type output_bit_offset = (plane_index*num_bits_per_plane);
-		
-	for(size_type index = 0; index < _length; ++index) {
+        for(size_type index = 0; index < _length; ++index) {
 
-	  size_type input_bit_offset = (type_width - num_bits_per_plane) - (index % num_planes)*num_bits_per_plane;
-	  size_type input_index = ((num_planes-1-plane_index)*segment_length) + index/num_planes;
-	  raw_type extracted_bits = (_input[input_index] >> input_bit_offset) & mask;
+          size_type input_bit_offset = (type_width - num_bits_per_plane) - (index % num_planes)*num_bits_per_plane;
+          size_type input_index = ((num_planes-1-plane_index)*segment_length) + index/num_planes;
+          raw_type extracted_bits = (_input[input_index] >> input_bit_offset) & mask;
 
-	  _output[index] = setbits_of_integertype(_output[index],extracted_bits,
-						  output_bit_offset,num_bits_per_plane);
-	}
+          _output[index] = setbits_of_integertype(_output[index],extracted_bits,
+                                                  output_bit_offset,num_bits_per_plane);
+        }
       }
 
+#pragma omp parallel for shared(_input, _output)    \
+  num_threads(num_threads)
       for(size_type index = 0; index < _length; ++index) {
-	_output[index] = xor_if_signed(rotate_right<1>(_output[index]));
+        _output[index] = xor_if_signed(rotate_right<1>(_output[index]));
       }
 
       return SUCCESS;
     }
 
-    
+
 
   };
 }; //sqeazy
--- src/cpp/src/encoders/bitswap_scheme_impl.hpp
+++ src/cpp/src/encoders/bitswap_scheme_impl.hpp
@@ -8,6 +8,10 @@
 #include "dynamic_stage.hpp"
 #include "string_parsers.hpp"
 
+#ifdef _OPENMP
+#include "omp.h"
+#endif
+
 #ifdef COMPASS_HAS_SSE4
 #include "bitplane_reorder_sse.hpp"
 #endif
@@ -19,17 +23,17 @@ namespace sqeazy {
   //TODO: remove static_num_bits_per_plane as soon as deprecated API is gone
   template <typename in_type,const unsigned static_num_bits_per_plane = 1>
   struct bitswap_scheme : public filter<in_type> {
-  
+
     typedef filter<in_type> base_type;
     typedef in_type raw_type;
     typedef in_type compressed_type;
 
     std::uint32_t num_bits_per_plane;
     std::uint32_t num_planes;
-    
+
     static_assert(std::is_arithmetic<raw_type>::value==true,"[bitswap_scheme] input type is non-arithmetic");
     static const std::string description() { return std::string("rewrite bitplanes of item as chunks of buffer, use <num_bits_per_plane|default = 1> to control how many bits each plane has"); };
-    
+
     //TODO: check syntax of lz4 configuration at runtime
     bitswap_scheme(const std::string& _payload=""):
       num_bits_per_plane(static_num_bits_per_plane),
@@ -40,18 +44,21 @@ namespace sqeazy {
       auto config_map = p.minors(_payload.begin(), _payload.end());
 
       if(config_map.size()){
-	auto f_itr = config_map.find("num_bits_per_plane");
-	std::uint32_t found_num_bits_per_plane = 0;
-	if(f_itr!=config_map.end())
-	  found_num_bits_per_plane = std::stoi(f_itr->second);
+        auto f_itr = config_map.find("num_bits_per_plane");
+        std::uint32_t found_num_bits_per_plane = 0;
+        if(f_itr!=config_map.end())
+          found_num_bits_per_plane = std::stoi(f_itr->second);
 
-	if(found_num_bits_per_plane!=num_bits_per_plane)
-	  std::cerr << "[bitswap_scheme] found num_bits_per_plane = " << found_num_bits_per_plane
-		    << " but was configured with " << static_num_bits_per_plane << ", proceed with caution!\n";
+        if(found_num_bits_per_plane!=num_bits_per_plane)
+          std::cerr << "[bitswap_scheme] found num_bits_per_plane = " << found_num_bits_per_plane
+                    << " but was configured with " << static_num_bits_per_plane << ", proceed with caution!\n";
       }
 
       const std::uint32_t raw_type_num_bits = sizeof(raw_type)*CHAR_BIT;
       num_planes = raw_type_num_bits/num_bits_per_plane;
+
+
+
     }
 
 
@@ -61,68 +68,68 @@ namespace sqeazy {
       msg << "bitswap"
 	  << num_bits_per_plane;
       return msg.str();
-	
+
     }
 
 
     /**
        \brief serialize the parameters of this filter
-     
+
        \return 
        \retval string .. that encodes the configuration paramters
-     
+
     */
     std::string config() const {
 
       std::ostringstream msg;
       msg << "num_bits_per_plane=" << std::to_string(num_bits_per_plane);
       return msg.str();
-    
+
     }
 
     std::intmax_t max_encoded_size(std::intmax_t _size_bytes) const override final {
-    
+
       return _size_bytes;
     }
 
     compressed_type* encode( const raw_type* _input, compressed_type* _output, std::size_t _length) override final {
 
-      
       std::size_t max_size = _length - (_length % num_planes);
       if(max_size < _length)
-	std::copy(_input+max_size,_input+_length,_output+max_size);
+        std::copy(_input+max_size,_input+_length,_output+max_size);
 
       int err = 0;
       if(sqeazy::platform::use_vectorisation::value &&
-	 compass::runtime::has(compass::feature::sse4()) &&
-	 num_bits_per_plane==1 &&
-	 sizeof(raw_type)>1)
-	{
-	  
+         compass::runtime::has(compass::feature::sse4()) &&
+         num_bits_per_plane==1 &&
+         sizeof(raw_type)>1)
+        {
+
 #ifdef _SQY_VERBOSE_
-	std::cout << "[bitswap_scheme::encode]\tusing see method\n";
+          std::cout << "[bitswap_scheme::encode]\tusing see method\n";
 #endif
-	err = sqeazy::detail::sse_bitplane_reorder_encode<static_num_bits_per_plane>(_input, 
-										     _output, 
-										     max_size);
-	
-      }
+          err = sqeazy::detail::sse_bitplane_reorder_encode<static_num_bits_per_plane>(_input,
+                                                                                       _output,
+                                                                                       max_size);
+
+        }
       else{
 #ifdef _SQY_VERBOSE_
-	std::cout << "[bitswap_scheme::encode]\tusing scalar method\n";
+        std::cout << "[bitswap_scheme::encode]\tusing scalar method\n";
 #endif
-	err = sqeazy::detail::scalar_bitplane_reorder_encode<static_num_bits_per_plane>(_input, 
-											_output, 
-											max_size);
+        err = sqeazy::detail::scalar_bitplane_reorder_encode<static_num_bits_per_plane>(_input,
+                                                                                        _output,
+                                                                                        max_size,
+                                                                                        this->n_threads());
       }
 
       if(!err)
-	return _output+(_length);
+        return _output+(_length);
       else
-	return nullptr;
+        return nullptr;
 
     }
-   
+
     compressed_type* encode( const raw_type* _input, compressed_type* _output, const std::vector<std::size_t>& _shape) override final {
 
       size_t _length = std::accumulate(_shape.begin(),
@@ -131,30 +138,32 @@ namespace sqeazy {
 				       std::multiplies<std::size_t>());
 
       return encode(_input,_output,_length);
-      
-      
+
+
     }
 
 
 
     int decode( const compressed_type* _input, raw_type* _output,
 		const std::vector<std::size_t>& _ishape,
-		std::vector<std::size_t> _oshape = std::vector<std::size_t>()) const override final {
+                std::vector<std::size_t> _oshape = std::vector<std::size_t>()) const override final {
 
       if(_oshape.empty())
-	_oshape = _ishape;
-      
+        _oshape = _ishape;
+
       typedef typename sqeazy::twice_as_wide<std::size_t>::type size_type;
       size_type _length = std::accumulate(_ishape.begin(),
-					  _ishape.end(),
-					  1,
-					  std::multiplies<std::size_t>());
+                                          _ishape.end(),
+                                          1,
+                                          std::multiplies<std::size_t>());
       size_type max_size = _length - (_length % num_planes);
       if(max_size < _length)
-	std::copy(_input+max_size,_input+_length,_output+max_size);
-      return sqeazy::detail::scalar_bitplane_reorder_decode<static_num_bits_per_plane>(_input, _output, max_size);
+        std::copy(_input+max_size,_input+_length,_output+max_size);
+      return sqeazy::detail::scalar_bitplane_reorder_decode<static_num_bits_per_plane>(_input,
+                                                                                       _output,
+                                                                                       max_size);
     }
-    
+
     int decode( const compressed_type* _input,
 		raw_type* _output,
 		std::size_t _length,
@@ -162,36 +171,31 @@ namespace sqeazy {
     {
 
       if(!_olength)
-	_olength = _length;
-      
+        _olength = _length;
+
       typedef typename sqeazy::twice_as_wide<std::size_t>::type size_type;
       size_type max_size = _length - (_length % num_planes);
       if(max_size < _length)
-	std::copy(_input+max_size,_input+_length,_output+max_size);
-      return sqeazy::detail::scalar_bitplane_reorder_decode<static_num_bits_per_plane>(_input, _output, max_size);
+        std::copy(_input+max_size,_input+_length,_output+max_size);
+      return sqeazy::detail::scalar_bitplane_reorder_decode<static_num_bits_per_plane>(_input,
+                                                                                       _output,
+                                                                                       max_size);
     }
-    
+
 
     ~bitswap_scheme(){};
 
     std::string output_type() const final override {
 
       return typeid(compressed_type).name();
-    
+
     }
 
     bool is_compressor() const final override {
-    
+
       return base_type::is_compressor;
-    
+
     }
-
-
-
-
-
-
-
 
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -234,7 +238,7 @@ namespace sqeazy {
       return static_encode(_input, _output, total_length);
     }
 
-    
+
     /**
      * @brief performs bitplane encoding, every bit of any value from _input is written to the respective 
      * compartment in _output, the granularity of the bit decomposition is given by static_num_bits_per_plane
@@ -255,7 +259,7 @@ namespace sqeazy {
       size_type max_size = _length - (_length % static_num_planes);
       if(max_size < _length)
 	std::copy(_input+max_size,_input+_length,_output+max_size);
-      
+
       if(sqeazy::platform::use_vectorisation::value && static_num_bits_per_plane==1 && sizeof(raw_type)>1){
 #ifdef _SQY_VERBOSE_
 	std::cout << "[bitplane encode]\tusing see method\n";
@@ -268,9 +272,9 @@ namespace sqeazy {
 #ifdef _SQY_VERBOSE_
 	std::cout << "[bitplane encode]\tusing scalar method\n";
 #endif	
-	return sqeazy::detail::scalar_bitplane_reorder_encode<static_num_bits_per_plane>(_input, 
-										  _output, 
-										  max_size);
+    return sqeazy::detail::scalar_bitplane_reorder_encode<static_num_bits_per_plane>(_input,
+                                                                                     _output,
+                                                                                     max_size);
       }
     }
 
