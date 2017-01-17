7cdcfeaf204910594043255f0973f7cdbc75fa39
Peter <steinbac@mpi-cbg.de>
2017-01-17 13:12:56 +0100

    updated ROOTDIR name of google benchmark

--- src/cpp/cmake/FindGoogleBenchmark.cmake
+++ src/cpp/cmake/FindGoogleBenchmark.cmake
@@ -2,37 +2,38 @@
 # - Try to find benchmark
 #
 # The following variables are optionally searched for defaults
-#  benchmark_ROOT_DIR:  Base directory where all benchmark components are found
+#  GOOGLEBENCHMARK_ROOT:  Base directory where all benchmark components are found
 #
 # Once done this will define
-#  benchmark_FOUND - System has benchmark
-#  benchmark_INCLUDE_DIRS - The benchmark include directories
-#  benchmark_LIBRARIES - The libraries needed to use benchmark
+#  googlebenchmark_FOUND - System has benchmark
+#  googlebenchmark_INCLUDE_DIR - The benchmark include directories
+#  googlebenchmark_LIBRARY - The libraries needed to use benchmark
 
-set(benchmark_ROOT_DIR "" CACHE PATH "Folder containing benchmark")
+set(GOOGLEBENCHMARK_ROOT "" CACHE PATH "Folder containing benchmark")
+#message("FindGoogleBenchmark :: ROOT dir given ${GOOGLEBENCHMARK_ROOT}")
 
-find_path(benchmark_INCLUDE_DIR "benchmark/benchmark.h"
-  PATHS ${benchmark_ROOT_DIR}
+find_path(googlebenchmark_INCLUDE_DIR "benchmark/benchmark.h"
+  PATHS ${GOOGLEBENCHMARK_ROOT}
   PATH_SUFFIXES include
   NO_DEFAULT_PATH)
-find_path(benchmark_INCLUDE_DIR "benchmark/benchmark.h")
+find_path(googlebenchmark_INCLUDE_DIR "benchmark/benchmark.h")
 
-find_library(benchmark_LIBRARY NAMES "benchmark"
-  PATHS ${benchmark_ROOT_DIR}
+find_library(googlebenchmark_LIBRARY NAMES "benchmark"
+  PATHS ${GOOGLEBENCHMARK_ROOT}
   PATH_SUFFIXES lib lib64
   NO_DEFAULT_PATH)
-find_library(benchmark_LIBRARY NAMES "benchmark")
+find_library(googlebenchmark_LIBRARY NAMES "benchmark")
 
 include(FindPackageHandleStandardArgs)
-# handle the QUIETLY and REQUIRED arguments and set benchmark_FOUND to TRUE
+# handle the QUIETLY and REQUIRED arguments and set googlebenchmark_FOUND to TRUE
 # if all listed variables are TRUE
-find_package_handle_standard_args(benchmark FOUND_VAR benchmark_FOUND
-  REQUIRED_VARS benchmark_LIBRARY
-  benchmark_INCLUDE_DIR)
+find_package_handle_standard_args(googlebenchmark 
+	FOUND_VAR googlebenchmark_FOUND
+	REQUIRED_VARS googlebenchmark_LIBRARY googlebenchmark_INCLUDE_DIR)
 
-if(benchmark_FOUND)
-  set(benchmark_LIBRARIES ${benchmark_LIBRARY})
-  set(benchmark_INCLUDE_DIRS ${benchmark_INCLUDE_DIR})
+if(googlebenchmark_FOUND)
+  set(googlebenchmark_LIBRARIES ${googlebenchmark_LIBRARY})
+  set(googlebenchmark_INCLUDE_DIRS ${googlebenchmark_INCLUDE_DIR})
 endif()
 
-mark_as_advanced(benchmark_INCLUDE_DIR benchmark_LIBRARY)
+mark_as_advanced(googlebenchmark_INCLUDE_DIR googlebenchmark_LIBRARY)
