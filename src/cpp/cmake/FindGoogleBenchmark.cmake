783237102db31a84c3ac703dfd3662f921a1d8ee
Peter Steinbach <steinbac@mpi-cbg.de>
2017-01-17 14:59:24 +0100

    created new variable *_AUX_LIBRARY to yield benchmark's external dependency on windows

--- src/cpp/cmake/FindGoogleBenchmark.cmake
+++ src/cpp/cmake/FindGoogleBenchmark.cmake
@@ -2,12 +2,14 @@
 # - Try to find benchmark
 #
 # The following variables are optionally searched for defaults
-#  GOOGLEBENCHMARK_ROOT:  Base directory where all benchmark components are found
+#  GOOGLEBENCHMARK_ROOT 		- Base directory where all benchmark components are found
 #
 # Once done this will define
-#  googlebenchmark_FOUND - System has benchmark
-#  googlebenchmark_INCLUDE_DIR - The benchmark include directories
-#  googlebenchmark_LIBRARY - The libraries needed to use benchmark
+#  googlebenchmark_FOUND 		- System has benchmark
+#  googlebenchmark_INCLUDE_DIR 	- The benchmark include directories
+#  googlebenchmark_LIBRARY 		- The libraries needed to use benchmark
+#  googlebenchmark_AUX_LIBRARY 	- The external libraries needed to use benchmark 
+#							  (only required on windows, filled with the location of shlwapi.lib)
 
 set(GOOGLEBENCHMARK_ROOT "" CACHE PATH "Folder containing benchmark")
 #message("FindGoogleBenchmark :: ROOT dir given ${GOOGLEBENCHMARK_ROOT}")
@@ -24,16 +26,25 @@ find_library(googlebenchmark_LIBRARY NAMES "benchmark"
   NO_DEFAULT_PATH)
 find_library(googlebenchmark_LIBRARY NAMES "benchmark")
 
+#set(googlebenchmark_AUX_LIBRARY "")
+if(WIN32)
+	find_library(googlebenchmark_AUX_LIBRARY NAMES shlwapi ShLwApi)
+	if(EXISTS ${googlebenchmark_AUX_LIBRARY})
+		message("-- [FindGoogleBenchmark] Windows detected, adding shlwapi.lib at ${googlebenchmark_AUX_LIBRARY}")
+	else()
+		message("-- [FindGoogleBenchmark] Windows detected, but shlwapi.lib not found")
+	endif()
+endif(WIN32)
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set googlebenchmark_FOUND to TRUE
 # if all listed variables are TRUE
 find_package_handle_standard_args(googlebenchmark 
 	FOUND_VAR googlebenchmark_FOUND
-	REQUIRED_VARS googlebenchmark_LIBRARY googlebenchmark_INCLUDE_DIR)
+	REQUIRED_VARS googlebenchmark_LIBRARY googlebenchmark_INCLUDE_DIR googlebenchmark_AUX_LIBRARY)
 
 if(googlebenchmark_FOUND)
-  set(googlebenchmark_LIBRARIES ${googlebenchmark_LIBRARY})
+  set(googlebenchmark_LIBRARIES ${googlebenchmark_LIBRARY} ${googlebenchmark_AUX_LIBRARY})
   set(googlebenchmark_INCLUDE_DIRS ${googlebenchmark_INCLUDE_DIR})
 endif()
 
-mark_as_advanced(googlebenchmark_INCLUDE_DIR googlebenchmark_LIBRARY)
+mark_as_advanced(googlebenchmark_INCLUDE_DIR googlebenchmark_LIBRARY googlebenchmark_AUX_LIBRARY)
