90d2bd11c897ad78d2d2fe34efe49976496750f6
Peter Steinbach <steinbac@mpi-cbg.de>
2017-01-19 15:53:37 +0100

    added multithread (produces segfault)

--- src/cpp/src/encoders/bitplane_reorder_sse.hpp
+++ src/cpp/src/encoders/bitplane_reorder_sse.hpp
@@ -2,6 +2,9 @@
 #define _BITPLANE_REORDER_SSE_H_
 
 
+#ifdef _OPENMP
+#include "omp.h"
+#endif
 
 #include "sqeazy_common.hpp"
 #include "sse_utils.hpp"
@@ -11,10 +14,10 @@ namespace sqeazy {
 
   namespace detail {
 
-    
+
     /**
        \brief bitplane reordering given sse register _block, new planes will be put into _reordered
-   
+
        \param[in] _block SSE block that contains X times of the type identified by T
        \param[out] _reordered bit planes to write reordered data into
        \param[in] _left_shift_output move the result number of bits (given by _left_shift_output) left 
@@ -33,13 +36,13 @@ namespace sqeazy {
        IMPORTANT: It is assumed that the items stored in __m128i originally were encoded as type T
        \return 
        \retval 
-   
+
     */
     template <unsigned plane_size, typename T>
     void reorder_bitplanes(const __m128i& _block, 
 			   std::vector<T*>& _reordered,
 			   const unsigned& _left_shift_output = 0
-			   ){
+                           ){
 
       static const unsigned type_width = sizeof(T)*CHAR_BIT;
       static const unsigned __m128i_in_bytes = 16;
@@ -52,8 +55,8 @@ namespace sqeazy {
       // static_assert((n_items_per_m128i*plane_size) % type_width == 0,
       // 		"[reorder_bitplanes]\t writing odd-sized planes to result array not implemented yet\n");
       static_assert(plane_size <= type_width,
-		    "[reorder_bitplanes]\t plane_size larger than input type. Doing nothing.\n");
-  
+                    "[reorder_bitplanes]\t plane_size larger than input type. Doing nothing.\n");
+
       float output_items_per_plane = n_items_per_m128i*plane_size/float(type_width);
 
       // unsigned reordered_byte = _reordered.size()*sizeof(T);
@@ -64,110 +67,110 @@ namespace sqeazy {
       // 	      << "\n";
       //   return;
       // }
-    
+
       const unsigned planes_per_output_segment = n_planes_per_T/_reordered.size();
       const unsigned bits_per_plane_on_output_item = type_width/planes_per_output_segment;
       static const unsigned char left_shift_to_msb = sizeof(int) - sizeof(T);
-  
+
       shift_left_m128i<T> left_shifter;
       int result = 0;
       T to_write_to_output = 0;
       std::size_t output_index = 0;
-  
+
       for(unsigned plane = 0;plane<(n_planes_per_T);plane+=plane_size){
 
-	result = 0;
-	to_write_to_output = 0;
-    
-	//left shift by plane to make the bit of interest the msb
-	__m128i input = left_shifter(_block,plane);
+        result = 0;
+        to_write_to_output = 0;
+
+        //left shift by plane to make the bit of interest the msb
+        __m128i input = left_shifter(_block,plane);
 
 
-	//aquire the leading bits of int32 blocks in m128
-	//m128 consists of 4 32bit integers that originate from T-bitwidth blocks
-	__m128i v_first_items =  _mm_slli_si128(
-						//convert to 32bits in order to get only the first (items 0,1,2,3 in _block) half of m128i
-						to_32bit_field<T>::conversion(input),
-						left_shift_to_msb //left shift by 2 bytes to move the bits towards the MSB
-						); 
+        //aquire the leading bits of int32 blocks in m128
+        //m128 consists of 4 32bit integers that originate from T-bitwidth blocks
+        __m128i v_first_items =  _mm_slli_si128(
+                                                //convert to 32bits in order to get only the first (items 0,1,2,3 in _block) half of m128i
+                                                to_32bit_field<T>::conversion(input),
+                                                left_shift_to_msb //left shift by 2 bytes to move the bits towards the MSB
+                                                );
 
-	//invert the sequence of v_first_items
-	v_first_items = _mm_shuffle_epi32(v_first_items, _MM_SHUFFLE(0,1,2,3));
-    
-	__m128 first_part = *reinterpret_cast<__m128*>(&v_first_items);
+        //invert the sequence of v_first_items
+        v_first_items = _mm_shuffle_epi32(v_first_items, _MM_SHUFFLE(0,1,2,3));
 
-	//collect the msb per 32bit item into result
-	result = _mm_movemask_ps (first_part); 
-	result <<= (n_items_per_m128i_half*plane_size);
+        __m128 first_part = *reinterpret_cast<__m128*>(&v_first_items);
+
+        //collect the msb per 32bit item into result
+        result = _mm_movemask_ps (first_part);
+        result <<= (n_items_per_m128i_half*plane_size);
 
 #ifdef WIN32
-	__m128 input_casted = _mm_castsi128_ps(input);
+        __m128 input_casted = _mm_castsi128_ps(input);
 #else
-	__m128 input_casted = reinterpret_cast<__m128>(input);
+        __m128 input_casted = reinterpret_cast<__m128>(input);
 #endif
 
-	//move second half of 128bits up
+        //move second half of 128bits up
 #ifdef WIN32
-	__m128i swapped_second_first = _mm_castps_si128(//swap second with first half of 128bits
-							_mm_movehl_ps(input_casted, input_casted)
-							);
+        __m128i swapped_second_first = _mm_castps_si128(//swap second with first half of 128bits
+                                                        _mm_movehl_ps(input_casted, input_casted)
+                                                        );
 #else
-	__m128i swapped_second_first = reinterpret_cast<__m128i>(//swap second with first half of 128bits
-								 _mm_movehl_ps(input_casted,input_casted)
-								 );
+        __m128i swapped_second_first = reinterpret_cast<__m128i>(//swap second with first half of 128bits
+                                                                 _mm_movehl_ps(input_casted,input_casted)
+                                                                 );
 #endif
-    
-	__m128i v_second_items = _mm_slli_si128(//convert to 32bits in order to get only the first (items 0,1,2,3 in _block) half of m128i
-						to_32bit_field<T>::conversion(swapped_second_first),
-						//left shift by n bytes to get the lower half of 32bits 
-						//(which is the higher half of the original)
-						left_shift_to_msb
-						);
 
-	//invert the sequence of v_second_items
-	v_second_items = _mm_shuffle_epi32(v_second_items, _MM_SHUFFLE(0,1,2,3));
+        __m128i v_second_items = _mm_slli_si128(//convert to 32bits in order to get only the first (items 0,1,2,3 in _block) half of m128i
+                                                to_32bit_field<T>::conversion(swapped_second_first),
+                                                //left shift by n bytes to get the lower half of 32bits
+                                                //(which is the higher half of the original)
+                                                left_shift_to_msb
+                                                );
 
-	//collect the msb per 32bit item into result
+        //invert the sequence of v_second_items
+        v_second_items = _mm_shuffle_epi32(v_second_items, _MM_SHUFFLE(0,1,2,3));
+
+        //collect the msb per 32bit item into result
 #ifdef WIN32
-	result += _mm_movemask_ps(_mm_castsi128_ps(v_second_items));
+        result += _mm_movemask_ps(_mm_castsi128_ps(v_second_items));
 #else
-	result += _mm_movemask_ps (reinterpret_cast<__m128>(v_second_items)) ; 
+        result += _mm_movemask_ps (reinterpret_cast<__m128>(v_second_items)) ;
 #endif
 
-	result <<= _left_shift_output;
-       
-    
+        result <<= _left_shift_output;
 
-	//dump the result to the output
-	output_index = planes_per_output_segment > 1 ? plane/planes_per_output_segment : plane;
 
-	if(!(output_items_per_plane>1)){
-	  //compute what needs to be written to the output
-	  if(planes_per_output_segment!=1){
-	    unsigned offset = ((plane % planes_per_output_segment)*bits_per_plane_on_output_item);
-	    to_write_to_output = T(result << offset);
-	  }
-	  else{
-	    to_write_to_output = T(result);
-	  }
-    
-	  *(_reordered[output_index]) += to_write_to_output;
 
-	}
-	else{
-	  int n_output_items_per_plane = std::round(output_items_per_plane);
-	  for(int out_offset = 0; out_offset < n_output_items_per_plane; ++out_offset)
-	    {
-	      *(_reordered[output_index]+n_output_items_per_plane-1-out_offset) += (result >> out_offset) & T(~0);
-	    }
-	}
+        //dump the result to the output
+        output_index = planes_per_output_segment > 1 ? plane/planes_per_output_segment : plane;
+
+        if(!(output_items_per_plane>1)){
+          //compute what needs to be written to the output
+          if(planes_per_output_segment!=1){
+            unsigned offset = ((plane % planes_per_output_segment)*bits_per_plane_on_output_item);
+            to_write_to_output = T(result << offset);
+          }
+          else{
+            to_write_to_output = T(result);
+          }
+
+          *(_reordered[output_index]) += to_write_to_output;
+
+        }
+        else{
+          int n_output_items_per_plane = std::round(output_items_per_plane);
+          for(int out_offset = 0; out_offset < n_output_items_per_plane; ++out_offset)
+            {
+              *(_reordered[output_index]+n_output_items_per_plane-1-out_offset) += (result >> out_offset) & T(~0);
+            }
+        }
 	
 
       }
 
     }
 
-    
+
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     //SSE implementation
     /*
@@ -208,7 +211,7 @@ namespace sqeazy {
       float output_items_per_m128 = nbits_per_plane*input_items_per_m128/float(raw_type_bitwidth);
       std::uint32_t advance_output_by = 1;
       std::uint32_t advance_output_every = 1;
-      
+
       if(output_items_per_m128<1){
 	advance_output_every = std::round(1./output_items_per_m128);
       } else {
@@ -219,7 +222,7 @@ namespace sqeazy {
       // 	std::cerr << "8-bit SSE accelerated bitswap not implemented yet\n";
       // 	return FAILURE;
       // }
-      
+
       //setup data structures for looping
       vec_xor<raw_type> xoring;
       vec_rotate_left<raw_type> rotate;
@@ -235,13 +238,13 @@ namespace sqeazy {
 	  xoring(&input);
 
 	input = rotate(&input);
-      
+
 	//bitplane reordering starts here
 	std::size_t shift_left_by = (count % advance_output_every == 0) ? raw_type_bitwidth/advance_output_every : 0;
 	reorder_bitplanes<nbits_per_plane>(input,
 					   output_ptr,
 					   shift_left_by);
-    
+
 	//moving the pointers to the output array forward
 	if(shift_left_by == 0){
 	  for(std::size_t i = 0;i<output_ptr.size();++i){
@@ -249,7 +252,7 @@ namespace sqeazy {
 	  }
 	}
 	
-      
+
       }
 
       return SUCCESS;
@@ -272,33 +275,35 @@ namespace sqeazy {
 	      typename size_type
 	      >
     static const error_code sse_bitplane_reorder_encode(const raw_type* _input,
-							raw_type* _output,
-							const size_type& _length)
+                                                        raw_type* _output,
+                                                        const size_type& _length,
+                                                        int _nthreads = 1)
     {
-      
+
       static const std::size_t n_bits_per_element = sizeof(raw_type)*CHAR_BIT;
       static const std::size_t n_elements_per_simd = 128/n_bits_per_element;
       static_assert((n_bits_per_element) % nbits_per_plane == 0,"\n\t[sse_bitplane_reorder_encode] number of bits per element not divisible byt number of bits per plane");
-      
+
       const std::size_t n_segments_per_element = n_bits_per_element/nbits_per_plane;
       static const std::size_t n_elements_full_sweep = n_elements_per_simd*n_segments_per_element;
-      
+
       if(_length % n_elements_full_sweep){
-	std::cerr << "[sse_bitplane_reorder_encode] " << _length << " ("<< n_bits_per_element
-		  <<" bits per element) cannot be processed by "
-		  << n_elements_full_sweep << " without rest\n";
+        std::cerr << "[sse_bitplane_reorder_encode] " << _length << " ("<< n_bits_per_element
+                  <<" bits per element) cannot be processed by "
+                  << n_elements_full_sweep << " without rest\n";
 	
-	return FAILURE;
+        return FAILURE;
       }
-      
+
       sqeazy::detail::simd_segment_broadcast(_input,
-					     _input + _length,
-					     _output);
-      
+                                             _input + _length,
+                                             _output,
+                                             _nthreads);
+
       return SUCCESS;
-      
+
     }
-    
+
   }//namespace detail
 
 }//namespace sqeazy
