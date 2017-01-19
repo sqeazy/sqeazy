eeac93162221fcd06e5a27976a54b75b6b0d54ad
Peter Steinbach <steinbac@mpi-cbg.de>
2017-01-19 17:13:31 +0100

    added dynamic benchmark fixture

--- src/cpp/bench/benchmark_fixtures.hpp
+++ src/cpp/bench/benchmark_fixtures.hpp
@@ -6,6 +6,7 @@
 #include <limits>
 #include <fstream>
 #include <sstream>
+#include <random>
 
 #include "traits.hpp"
 #include "benchmark/benchmark.h"
@@ -14,6 +15,131 @@
 namespace sqeazy {
 
   namespace benchmark {
+
+    template <typename T = std::uint16_t
+              >
+    struct dynamic_synthetic_data : public ::benchmark::Fixture
+    {
+      std::vector<T> sinus_;
+      std::vector<T> embryo_;
+      std::vector<T> noisy_embryo_;
+      std::vector<T> output_;
+
+      std::vector<std::size_t> shape_;
+      std::size_t size_;
+
+      void setup(std::size_t _size){
+
+        shape_[sqeazy::row_major::x] = 1 << int(std::round(std::log2(std::pow(_size,1/3.f))));
+        shape_[sqeazy::row_major::y] = shape_[sqeazy::row_major::x];
+        shape_[sqeazy::row_major::z] = std::floor(_size/(shape_[sqeazy::row_major::y]*shape_[sqeazy::row_major::x]));
+
+        size_ = std::accumulate(shape_.begin(), shape_.end(), 1, std::multiplies<std::size_t>());
+
+        setup_from_size();
+
+      }
+
+      template <typename value_type>
+      void setup(const std::vector<value_type>& _shape){
+
+        shape_ = _shape;
+
+        size_ = std::accumulate(shape_.begin(), shape_.end(), 1, std::multiplies<std::size_t>());
+
+        setup_from_size();
+
+      }
+
+      void setup_from_size(){
+
+        sinus_.clear();
+        embryo_.clear();
+        noisy_embryo_.clear();
+        output_.clear();
+
+        sinus_.resize(size_);
+        embryo_.resize(size_);
+        noisy_embryo_.resize(size_);
+        output_.resize(size_);
+
+        float factor_frequency = shape_.front();
+        static const float scale = .25f*std::numeric_limits<T>::max();
+
+        //sinus_
+        unsigned index = 0;
+        for( T& _element : sinus_ ){
+          _element = scale*(1+std::sin(factor_frequency*index++));
+        }
+
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        const T signal_intensity_ = std::numeric_limits<T>::max()*.6;
+        std::exponential_distribution<> dis(1.f/(.01f*signal_intensity_));
+
+        for(uint32_t idx = 0;idx < noisy_embryo_.size();++idx)
+          noisy_embryo_.data()[idx] += dis(gen);
+
+        float ellipsoid = 0;
+        float dist_to_1 = 0;
+
+        auto embryo_itr = embryo_.begin();
+        auto noisy_itr = noisy_embryo_.begin();
+
+        for(uint32_t z=0;z<shape_[sqeazy::row_major::z];z++)
+          for(uint32_t y=0;y<shape_[sqeazy::row_major::y];y++)
+            for(uint32_t x=0;x<shape_[sqeazy::row_major::x];x++){
+
+              //shift to center and square
+              float x_2 = (float(x) - shape_[sqeazy::row_major::x]/2.f);x_2 *= x_2;
+              float y_2 = (float(y) - shape_[sqeazy::row_major::y]/2.f);y_2 *= y_2;
+              float z_2 = (float(z) - shape_[sqeazy::row_major::z]/2.f);z_2 *= z_2;
+
+              ellipsoid =  x_2/(shape_[sqeazy::row_major::x]*shape_[sqeazy::row_major::x]*.25*.25);
+              ellipsoid += y_2/(shape_[sqeazy::row_major::y]*shape_[sqeazy::row_major::y]*.25*.25);
+              ellipsoid += z_2/(shape_[sqeazy::row_major::z]*shape_[sqeazy::row_major::z]*.6*.6);
+
+              dist_to_1 = std::abs(1 - ellipsoid);
+              if ( dist_to_1 < .07 ){
+
+                *(embryo_itr++) = signal_intensity_;
+                *(noisy_itr++) += signal_intensity_;
+              }
+
+            }
+
+
+
+
+
+      }
+
+      dynamic_synthetic_data():
+        sinus_(),
+        embryo_(),
+        noisy_embryo_(),
+        output_(),
+        shape_(3,0),
+        size_()
+      {}
+
+      void SetUp(const ::benchmark::State& st) {
+        //m = ConstructRandomMap(st.range(0));
+        setup(st.range(0));
+
+      }
+
+      void TearDown(const ::benchmark::State&) {  }
+
+      unsigned long data_in_byte() const {
+        return size_*sizeof(T);
+      }
+
+
+      virtual void BenchmarkCase(::benchmark::State&){}
+
+    };
+
 
     template <typename T = unsigned short,
               //obtained by `getconf -a /usr|grep -i CACHE` on a Intel(R) Core(TM) i7-3520M
@@ -97,6 +223,9 @@ namespace sqeazy {
         return sin_data.size()*sizeof(T);
       }
 
+
+      virtual void BenchmarkCase(::benchmark::State&){}
+
       friend std::ostream& operator<<(std::ostream& _cout, const static_synthetic_data& _self){
         _cout << "[x,y,z] "
               << _self.axis_length(sqeazy::row_major::x) <<"x"
@@ -106,7 +235,6 @@ namespace sqeazy {
         return _cout;
       }
 
-      virtual void BenchmarkCase(::benchmark::State&){}
     };
 
 
--- src/cpp/bench/test_benchmark_fixture_impl.cpp
+++ src/cpp/bench/test_benchmark_fixture_impl.cpp
@@ -6,17 +6,13 @@
 #include "benchmark_fixtures.hpp"
 
 typedef sqeazy::benchmark::static_synthetic_data<> default_static_synthetic_data;
+typedef sqeazy::benchmark::dynamic_synthetic_data<> default_dynamic_synthetic_data;
 
 BOOST_FIXTURE_TEST_SUITE( fundamentals , default_static_synthetic_data )
 
 BOOST_AUTO_TEST_CASE( fixture_can_be_setup ){
 
   BOOST_CHECK(!shape.empty());
-
-  // std::cout << "x,y,z = "
-  //           << shape[sqeazy::row_major::x] << ","
-  //           << shape[sqeazy::row_major::y] << ","
-  //           << shape[sqeazy::row_major::z] << "\n";
 
   BOOST_CHECK(shape[sqeazy::row_major::x] % 2 == 0);
   BOOST_CHECK_GT(shape[sqeazy::row_major::x], 0);
@@ -26,6 +22,26 @@ BOOST_AUTO_TEST_CASE( fixture_can_be_setup ){
 
   BOOST_CHECK(shape[sqeazy::row_major::z] % 2 == 0);
   BOOST_CHECK_GT(shape[sqeazy::row_major::z], 0);
+
+}
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_FIXTURE_TEST_SUITE( dynamic_fundamentals , default_dynamic_synthetic_data )
+
+BOOST_AUTO_TEST_CASE( setup_works ){
+
+  BOOST_CHECK(!shape_.empty());
+
+  setup(1024);
+
+  BOOST_CHECK(shape_[sqeazy::row_major::x] % 2 == 0);
+  BOOST_CHECK_GT(shape_[sqeazy::row_major::x], 0);
+
+  BOOST_CHECK(shape_[sqeazy::row_major::y] % 2 == 0);
+  BOOST_CHECK_GT(shape_[sqeazy::row_major::y], 0);
+
+  BOOST_CHECK(shape_[sqeazy::row_major::z] % 2 == 0);
+  BOOST_CHECK_GT(shape_[sqeazy::row_major::z], 0);
 
 }
 BOOST_AUTO_TEST_SUITE_END()
