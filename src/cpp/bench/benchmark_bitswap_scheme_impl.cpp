5dde5f83e57cad36ea2f8d3e988aa847714f0520
Peter Steinbach <steinbac@mpi-cbg.de>
2017-01-19 15:51:55 +0100

    some renaming, added new benchmarks, changed time according to benchmark wiki

--- src/cpp/bench/benchmark_bitswap_scheme_impl.cpp
+++ src/cpp/bench/benchmark_bitswap_scheme_impl.cpp
@@ -3,21 +3,87 @@
 #include "encoders/bitswap_scheme_impl.hpp"
 #include "benchmark_fixtures.hpp"
 
-typedef sqeazy::benchmark::static_synthetic_data<> default_fixture;
+typedef sqeazy::benchmark::static_synthetic_data<> static_default_fixture;
 
-BENCHMARK_DEFINE_F(default_fixture, one_vs_two_threads)(benchmark::State& st) {
+BENCHMARK_DEFINE_F(static_default_fixture, one_vs_two_threads)(benchmark::State& state) {
 
   sqeazy::bitswap_scheme<std::uint16_t> local;
-  local.set_n_threads(st.threads);
+  local.set_n_threads(state.threads);
 
-  while (st.KeepRunning()) {
+  local.encode(sin_data.data(),
+                 output_data.data(),
+                 size);
+
+  while (state.KeepRunning()) {
+
+    state.PauseTiming();
+    std::fill(output_data.begin(), output_data.end(),0);
+    state.ResumeTiming();
+
     local.encode(sin_data.data(),
                  output_data.data(),
                  size);
   }
 
+  state.SetBytesProcessed(int64_t(state.iterations()) *
+                          int64_t(sin_data.size())*sizeof(sin_data.front()));
 }
 
-BENCHMARK_REGISTER_F(default_fixture, one_vs_two_threads)->Threads(2)->Threads(1);
+BENCHMARK_REGISTER_F(static_default_fixture, one_vs_two_threads)->Threads(2)->Threads(1)->UseRealTime();
+
+BENCHMARK_DEFINE_F(static_default_fixture, scalar_one_vs_two_threads)(benchmark::State& state) {
+
+  sqeazy::bitswap_scheme<std::uint16_t> local;
+  local.set_n_threads(state.threads);
+
+
+  //heat the caches
+  sqeazy::detail::scalar_bitplane_reorder_encode<1>(sin_data.data(),
+                                                    output_data.data(),
+                                                    sin_data.size(),
+                                                    state.threads);
+
+  while (state.KeepRunning()) {
+    state.PauseTiming();
+    std::fill(output_data.begin(), output_data.end(),0);
+    state.ResumeTiming();
+
+    sqeazy::detail::scalar_bitplane_reorder_encode<1>(sin_data.data(),
+                                                      output_data.data(),
+                                                      sin_data.size(),
+                                                      state.threads);
+  }
+
+  state.SetBytesProcessed(int64_t(state.iterations()) *
+                          int64_t(sin_data.size())*sizeof(sin_data.front()));
+}
+
+BENCHMARK_REGISTER_F(static_default_fixture, scalar_one_vs_two_threads)->Threads(2)->Threads(1)->UseRealTime();
+
+BENCHMARK_DEFINE_F(static_default_fixture, sse_one_vs_two_threads)(benchmark::State& state) {
+
+  //heat the caches
+  sqeazy::detail::sse_bitplane_reorder_encode<1>(sin_data.data(),
+                                                 output_data.data(),
+                                                 sin_data.size(),
+                                                 state.threads);
+
+  while (state.KeepRunning()) {
+    state.PauseTiming();
+    std::fill(output_data.begin(), output_data.end(),0);
+    state.ResumeTiming();
+
+    sqeazy::detail::sse_bitplane_reorder_encode<1>(sin_data.data(),
+                                                      output_data.data(),
+                                                      sin_data.size(),
+                                                      state.threads);
+  }
+
+  state.SetBytesProcessed(int64_t(state.iterations()) *
+                          int64_t(sin_data.size())*sizeof(sin_data.front()));
+}
+
+BENCHMARK_REGISTER_F(static_default_fixture, sse_one_vs_two_threads)->Threads(2)->Threads(1)->UseRealTime();
+
 
 BENCHMARK_MAIN();
