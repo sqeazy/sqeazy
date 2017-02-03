536d265e7d387d86dc2ac2aca1b2b8bc8f3fc604
Peter Steinbach <steinbac@mpi-cbg.de>
2017-02-03 16:44:48 +0100

    fixed wrong variable references, added notes

--- src/cpp/bench/benchmark_frame_shuffle_scheme_impl.cpp
+++ src/cpp/bench/benchmark_frame_shuffle_scheme_impl.cpp
@@ -1,5 +1,12 @@
 #define __BENCHMARK_FRAME_SHUFFLE_SCHEME_IMPL_CPP__
 
+/*
+  notes/TODOs:
+  - [2017-02-03] performance of frame_shuffle appears to be stuck at 80 MB/s, first profiling pointed at boost::accumulators to be the cause
+  - [2017-02-03] potential other bottleneck could be the single threaded std::sort containted therein
+
+*/
+
 #include <thread>
 
 #include "encoders/frame_shuffle_scheme_impl.hpp"
@@ -86,65 +93,99 @@ BENCHMARK_REGISTER_F(static_default_fixture, scalar_two_threads)->UseRealTime();
 
 // BENCHMARK_REGISTER_F(static_default_fixture, sse_one_vs_two_threads)->Threads(2)->Threads(1)->UseRealTime();
 
-// BENCHMARK_DEFINE_F(dynamic_default_fixture, single_thread)(benchmark::State& state) {
+BENCHMARK_DEFINE_F(dynamic_default_fixture, single_thread)(benchmark::State& state) {
 
 
-//   if (state.thread_index == 0) {
-//     SetUp(state);
-//   }
+  if (state.thread_index == 0) {
+    SetUp(state);
+  }
 
-//   //heat the caches
-//   sqeazy::detail::sse_bitplane_reorder_encode<1>(sinus_.data(),
-//                                                  output_.data(),
-//                                                  sinus_.size(),
-//                                                  state.threads);
-
-//   while (state.KeepRunning()) {
-//     state.PauseTiming();
-//     std::fill(output_.begin(), output_.end(),0);
-//     state.ResumeTiming();
-
-//     sqeazy::detail::sse_bitplane_reorder_encode<1>(sinus_.data(),
-//                                                    output_.data(),
-//                                                    sinus_.size(),
-//                                                    state.threads);
-//   }
-
-//   state.SetBytesProcessed(int64_t(state.iterations()) *
-//                           int64_t(size_)*sizeof(sinus_.front()));
-// }
-
-// BENCHMARK_REGISTER_F(dynamic_default_fixture, single_thread)->Range(1 << 16,1 << 25);
-
-// BENCHMARK_DEFINE_F(dynamic_default_fixture, two_threads)(benchmark::State& state) {
+  sqeazy::frame_shuffle_scheme<std::uint16_t> local;
+  local.set_n_threads(1);
 
 
-//   if (state.thread_index == 0) {
-//     SetUp(state);
-//   }
+  local.encode(sinus_.data(),
+               output_.data(),
+               shape_);
 
-//   //heat the caches
-//   sqeazy::detail::sse_bitplane_reorder_encode<1>(sinus_.data(),
-//                                                  output_.data(),
-//                                                  sinus_.size(),
-//                                                  2);
+  while (state.KeepRunning()) {
+    state.PauseTiming();
+    std::fill(output_.begin(), output_.end(),0);
+    state.ResumeTiming();
 
-//   while (state.KeepRunning()) {
-//     state.PauseTiming();
-//     std::fill(output_.begin(), output_.end(),0);
-//     state.ResumeTiming();
+    local.encode(sinus_.data(),
+               output_.data(),
+               shape_);
+  }
 
-//     sqeazy::detail::sse_bitplane_reorder_encode<1>(sinus_.data(),
-//                                                    output_.data(),
-//                                                    sinus_.size(),
-//                                                    2);
-//   }
+  state.SetBytesProcessed(int64_t(state.iterations()) *
+                          int64_t(size_)*sizeof(sinus_.front()));
+}
 
-//   state.SetBytesProcessed(int64_t(state.iterations()) *
-//                           int64_t(size_)*sizeof(sinus_.front()));
-// }
+BENCHMARK_REGISTER_F(dynamic_default_fixture, single_thread)->Range(1 << 16,1 << 25);
 
-// BENCHMARK_REGISTER_F(dynamic_default_fixture, two_threads)->UseRealTime()->Range(1 << 16,1 << 25);
+BENCHMARK_DEFINE_F(dynamic_default_fixture, two_threads)(benchmark::State& state) {
+
+
+  if (state.thread_index == 0) {
+    SetUp(state);
+  }
+
+  sqeazy::frame_shuffle_scheme<std::uint16_t> local;
+  local.set_n_threads(2);
+
+
+  local.encode(sinus_.data(),
+               output_.data(),
+               shape_);
+
+  while (state.KeepRunning()) {
+    state.PauseTiming();
+    std::fill(output_.begin(), output_.end(),0);
+    state.ResumeTiming();
+
+    local.encode(sinus_.data(),
+               output_.data(),
+               shape_);
+  }
+
+  state.SetBytesProcessed(int64_t(state.iterations()) *
+                          int64_t(size_)*sizeof(sinus_.front()));
+}
+
+BENCHMARK_REGISTER_F(dynamic_default_fixture, two_threads)->UseRealTime()->Range(1 << 16,1 << 25);
+
+BENCHMARK_DEFINE_F(dynamic_default_fixture, max_threads)(benchmark::State& state) {
+
+
+  if (state.thread_index == 0) {
+    SetUp(state);
+  }
+
+  int nthreads = std::thread::hardware_concurrency();
+  sqeazy::frame_shuffle_scheme<std::uint16_t> local;
+  local.set_n_threads(nthreads);
+
+
+  local.encode(sinus_.data(),
+               output_.data(),
+               shape_);
+
+  while (state.KeepRunning()) {
+    state.PauseTiming();
+    std::fill(output_.begin(), output_.end(),0);
+    state.ResumeTiming();
+
+    local.encode(sinus_.data(),
+               output_.data(),
+               shape_);
+  }
+
+  state.SetBytesProcessed(int64_t(state.iterations()) *
+                          int64_t(size_)*sizeof(sinus_.front()));
+}
+
+BENCHMARK_REGISTER_F(dynamic_default_fixture, max_threads)->UseRealTime()->Range(1 << 16,1 << 25);
 
 // BENCHMARK_DEFINE_F(dynamic_default_fixture, max_threads)(benchmark::State& state) {
 
