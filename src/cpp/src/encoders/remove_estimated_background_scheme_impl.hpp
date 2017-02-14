d4a5e3defc9a8c141ca5bd0795240a63a06368b7
Peter Steinbach <steinbac@mpi-cbg.de>
2017-02-14 11:37:12 +0100

    created benchmark for remove_estimated_background

--- src/cpp/src/encoders/remove_estimated_background_scheme_impl.hpp
+++ src/cpp/src/encoders/remove_estimated_background_scheme_impl.hpp
@@ -11,22 +11,25 @@
 #include "string_parsers.hpp"
 #include "background_scheme_utils.hpp"
 
+#include "flatten_to_neighborhood_scheme_impl.hpp"
+#include "remove_background_scheme_impl.hpp"
+
 namespace sqeazy {
 
-  
+
   template <typename in_type>
   struct remove_estimated_background_scheme : public filter<in_type> {
-  
+
     typedef filter<in_type> base_type;
     typedef in_type raw_type;
     typedef in_type compressed_type;
-    
+
     remove_estimated_background_scheme(const std::string& _payload=""){}
     static const std::string description() { return std::string("estimate noise from darkest planes, remove median of that plane from all items"); };
-    
+
     std::string name() const override final {
 
-        return std::string("rmestbkrd");
+      return std::string("rmestbkrd");
     }
 
     std::string config() const override final {
@@ -34,43 +37,43 @@ namespace sqeazy {
     }
 
 
-    
+
     std::intmax_t max_encoded_size(std::intmax_t _size_bytes) const override final {
-    
+
       return _size_bytes;
     }
-    
+
     ~remove_estimated_background_scheme(){};
 
     std::string output_type() const final override {
 
       return typeid(compressed_type).name();
-    
+
     }
 
     bool is_compressor() const final override {
-    
+
       return base_type::is_compressor;
-    
+
     }
 
 
     compressed_type* encode( const raw_type* _input, compressed_type* _output, std::size_t _input_size) override final {
 
       std::vector<std::size_t> shape = {_input_size};
-      
+
       return encode(_input,_output,shape);
-      
+
     }
-    
+
     compressed_type* encode( const raw_type* _input, compressed_type* _output, const std::vector<std::size_t>& _shape) override final {
 
       std::vector<raw_type> darkest_face;
       extract_darkest_face((const raw_type*)_input, _shape, darkest_face);
-        
+
       sqeazy::histogram<raw_type> t;
       t.add_from_image(darkest_face.begin(), darkest_face.end());
-	
+
       std::size_t input_length = std::accumulate(_shape.begin(), _shape.end(), 1, std::multiplies<std::size_t>());
 
       const float reduce_by = t.calc_support(.99f);
@@ -78,24 +81,24 @@ namespace sqeazy {
 #ifdef _SQY_VERBOSE_
       std::cout << "[SQY_VERBOSE] remove_estimated_background ";
       for(short i = 0;i<_shape.size();++i){
-	std::cout << _shape[i] << ((_shape[i]!=_shape.back()) ? "x" : ", ");
+        std::cout << _shape[i] << ((_shape[i]!=_shape.back()) ? "x" : ", ");
       }
-	
+
       std::cout << " darkest face: backgr_estimate = " << reduce_by << "\n";
       t.fill_stats();
       std::cout << "[SQY_VERBOSE] " << histogram<raw_type>::print_header() << "\n[SQY_VERBOSE] " << t << "\n";
-	
+
 #endif
 
       if(_output) {
-	//copies the input to output, skipping pixels that have a neighborhood complying crirteria
-	flatten_to_neighborhood_scheme<raw_type>::static_encode(_input, _output, _shape, reduce_by);
-	//set those pixels to 0 that fall below reduce_by
-	remove_background_scheme<raw_type>::static_encode_inplace(_output, input_length, reduce_by);
+        //copies the input to output, skipping pixels that have a neighborhood complying crirteria
+        flatten_to_neighborhood_scheme<raw_type>::static_encode(_input, _output, _shape, reduce_by);
+        //set those pixels to 0 that fall below reduce_by
+        remove_background_scheme<raw_type>::static_encode_inplace(_output, input_length, reduce_by);
       }
       else {
-	std::cerr << "WARNING ["<< static_name() <<"::encode]\t inplace operation not supported\n";
-	return nullptr;
+        std::cerr << "WARNING ["<< static_name() <<"::encode]\t inplace operation not supported\n";
+        return nullptr;
       }
 
       return _output+input_length;
@@ -105,44 +108,44 @@ namespace sqeazy {
     /**
        \brief decode method basically does a copy only as we cannot infer the estimated background level (yet)
 
-       \param[in] 
+       \param[in]
 
-       \return 
-       \retval 
+       \return
+       \retval
 
     */
     int decode( const compressed_type* _input,
-		raw_type* _output,
-		std::size_t _input_size,
-		std::size_t _output_size = 0) const override final {
+                raw_type* _output,
+                std::size_t _input_size,
+                std::size_t _output_size = 0) const override final {
 
       std::copy(_input, _input + _input_size,_output);
-      
+
       return 0;
     }
 
     /**
        \brief decode method basically does a copy only as we cannot infer the estimated background level (yet)
 
-       \param[in] 
+       \param[in]
 
-       \return 
-       \retval 
+       \return
+       \retval
 
     */
     int decode( const compressed_type* _input, raw_type* _output,
-		const std::vector<std::size_t>& _shape,
-		std::vector<std::size_t>) const override final {
+                const std::vector<std::size_t>& _shape,
+                std::vector<std::size_t>) const override final {
 
       std::size_t length = std::accumulate(_shape.begin(), _shape.end(), 1, std::multiplies<std::size_t>());
       return decode(_input,_output,length);
     }
 
-    
+
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // DEPRECATED API
-    
-        static const bool is_sink = false;
+
+    static const bool is_sink = false;
 
     /**
      * @brief producing the name of this scheme and return it as a string
@@ -152,7 +155,7 @@ namespace sqeazy {
     static const std::string static_name() {
 
 
-        return std::string("rmestbkrd");
+      return std::string("rmestbkrd");
 
     }
 
@@ -170,18 +173,18 @@ namespace sqeazy {
      */
     static const void mean_and_var(ItrT begin, ItrT end, float& _mean, float& _var) {
 
-        unsigned long length = end - begin;
-        float sum = 0.f;
-        float sum_of_squares = 0.f;
+      unsigned long length = end - begin;
+      float sum = 0.f;
+      float sum_of_squares = 0.f;
 
-        for(; begin!=end; ++begin) {
-            sum += float(*begin);
-            sum_of_squares += float(*begin) * float(*begin);
-        }
+      for(; begin!=end; ++begin) {
+        sum += float(*begin);
+        sum_of_squares += float(*begin) * float(*begin);
+      }
 
-        _mean = sum/length;
-        _var = std::sqrt((sum_of_squares/length) - (_mean*_mean));
-	
+      _mean = sum/length;
+      _var = std::sqrt((sum_of_squares/length) - (_mean*_mean));
+
     }
 
     template <typename size_type>
@@ -197,43 +200,43 @@ namespace sqeazy {
      * @return sqeazy::error_code
      */
     static const error_code static_encode(raw_type* _input,
-                                   compressed_type* _output,
-                                   const std::vector<size_type>& _dims)
-    {
+                                          compressed_type* _output,
+                                          const std::vector<size_type>& _dims)
+      {
 
 
         std::vector<raw_type> darkest_face;
         extract_darkest_face((const raw_type*)_input, _dims, darkest_face);
-        
-	sqeazy::histogram<raw_type> t;
-	t.add_from_image(darkest_face.begin(), darkest_face.end());
-	
-	unsigned long input_length = std::accumulate(_dims.begin(), _dims.end(), 1, std::multiplies<size_type>());
+
+        sqeazy::histogram<raw_type> t;
+        t.add_from_image(darkest_face.begin(), darkest_face.end());
+
+        unsigned long input_length = std::accumulate(_dims.begin(), _dims.end(), 1, std::multiplies<size_type>());
 
         const float reduce_by = t.calc_support(.99f);
 
-	#ifdef _SQY_VERBOSE_
-	std::cout << "[SQY_VERBOSE] remove_estimated_background ";
-	for(short i = 0;i<_dims.size();++i){
-	  std::cout << _dims[i] << ((_dims[i]!=_dims.back()) ? "x" : ", ");
-	}
-	
-	std::cout << " darkest face: backgr_estimate = " << reduce_by << "\n";
-	t.fill_stats();
-	std::cout << "[SQY_VERBOSE] " << histogram<raw_type>::print_header() << "\n[SQY_VERBOSE] " << t << "\n";
-	
-	#endif
+#ifdef _SQY_VERBOSE_
+        std::cout << "[SQY_VERBOSE] remove_estimated_background ";
+        for(short i = 0;i<_dims.size();++i){
+          std::cout << _dims[i] << ((_dims[i]!=_dims.back()) ? "x" : ", ");
+        }
+
+        std::cout << " darkest face: backgr_estimate = " << reduce_by << "\n";
+        t.fill_stats();
+        std::cout << "[SQY_VERBOSE] " << histogram<raw_type>::print_header() << "\n[SQY_VERBOSE] " << t << "\n";
+
+#endif
 
         if(_output) {
-	    //copies the input to output, skipping pixels that have a neighborhood complying crirteria
-            flatten_to_neighborhood_scheme<raw_type>::static_encode(_input, _output, _dims, reduce_by);
-	    //set those pixels to 0 that fall below reduce_by
-            remove_background_scheme<raw_type>::static_encode_inplace(_output, input_length, reduce_by);
+          //copies the input to output, skipping pixels that have a neighborhood complying crirteria
+          flatten_to_neighborhood_scheme<raw_type>::static_encode(_input, _output, _dims, reduce_by);
+          //set those pixels to 0 that fall below reduce_by
+          remove_background_scheme<raw_type>::static_encode_inplace(_output, input_length, reduce_by);
         }
         else {
-            std::cerr << "WARNING ["<< static_name() <<"::encode]\t inplace operation requested, flatten_to_neighborhood skipped\n";
-	    //set those pixels to 0 that fall below reduce_by
-            remove_background_scheme<raw_type>::static_encode_inplace(_input, input_length, reduce_by);
+          std::cerr << "WARNING ["<< static_name() <<"::encode]\t inplace operation requested, flatten_to_neighborhood skipped\n";
+          //set those pixels to 0 that fall below reduce_by
+          remove_background_scheme<raw_type>::static_encode_inplace(_input, input_length, reduce_by);
 
         }
 
@@ -241,7 +244,7 @@ namespace sqeazy {
 
 
         return SUCCESS;
-    }
+      }
 
     template <typename SizeType>
     /**
@@ -251,28 +254,28 @@ namespace sqeazy {
      * @return sqeazy::error_code
      */
     static const error_code static_decode(const compressed_type* _input,
-                                   raw_type* _output,
-                                   const SizeType& _length)
-    {
+                                          raw_type* _output,
+                                          const SizeType& _length)
+      {
         std::copy(_input, _input + _length, _output);
         return SUCCESS;
-    }
+      }
 
     template <typename SizeType>
     /**
-    * @brief reconstructing the background removal from an estimate is impossible (so far),
-    * therefor the input buffer is copied to the output buffer of size given by dimensionality
-    *
-    * @return sqeazy::error_code
-    */
+     * @brief reconstructing the background removal from an estimate is impossible (so far),
+     * therefor the input buffer is copied to the output buffer of size given by dimensionality
+     *
+     * @return sqeazy::error_code
+     */
     static const error_code static_decode(const compressed_type* _input,
-                                   raw_type* _output,
-                                   const std::vector<SizeType>& _length)
-    {
+                                          raw_type* _output,
+                                          const std::vector<SizeType>& _length)
+      {
         unsigned long total_size = std::accumulate(_length.begin(), _length.end(), 1, std::multiplies<SizeType>());
 
         return static_decode(_input, _output, total_size);
-    }
+      }
 
 
 
