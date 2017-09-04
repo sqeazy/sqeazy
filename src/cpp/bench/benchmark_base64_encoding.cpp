27ec687fdb6433b70d49792d94244e287e069842
Peter Steinbach <steinbac@mpi-cbg.de>
2017-09-04 09:35:03 +0200

    added base64 based benchmarks comparing the boost implementation to mine; (my) implementation based on the wikipedia page is 10x faster than the boost based one

--- src/cpp/bench/benchmark_base64_encoding.cpp
+++ src/cpp/bench/benchmark_base64_encoding.cpp
@@ -1,38 +1,108 @@
 #define __BENCHMARK_BASE64_ENCODING_CPP__
 
 #include <thread>
+#include <string>
+#include <cmath>
+#include <iostream>
 
-#include "encoders/base64.hpp"
-#include "benchmark_fixtures.hpp"
+#include "base64.hpp"
+#include "benchmark/benchmark.h"
 
-typedef sqeazy::benchmark::static_synthetic_data<> static_default_fixture;
-typedef sqeazy::benchmark::dynamic_synthetic_data<> dynamic_default_fixture;
 
-BENCHMARK_DEFINE_F(dynamic_default_fixture, single_thread)(benchmark::State& state) {
+
+struct synthetic_data : public ::benchmark::Fixture
+{
+
+  std::size_t size_;
+  std::string input_;
+  std::string output_;
+
+
+
+  void setup(std::size_t _size){
+
+    input_.clear();
+    input_.resize(_size);
+    output_.resize(std::ceil(_size/3.)*4);
+    size_ = _size;
+
+    if(_size == 0)
+      return;
+
+    char element = -127;
+    for(char& el : input_)
+      el = element++;
+
+  }
+
+  synthetic_data():
+    size_(0),
+    input_(),
+    output_(){
+
+    setup(size_);
+  }
+
+  void SetUp(const ::benchmark::State& st) {
+    setup(st.range(0));
+  }
+
+  void TearDown(const ::benchmark::State&) {  }
+
+
+  virtual void BenchmarkCase(::benchmark::State& st){
+    setup(st.range(0));
+  }
+
+};
+
+
+BENCHMARK_DEFINE_F(synthetic_data, BM_single_thread_boost)(benchmark::State& state) {
 
 
   if (state.thread_index == 0) {
     SetUp(state);
   }
 
-  local.set_n_threads(1);
+  while (state.KeepRunning()) {
+    state.PauseTiming();
+    std::fill(output_.begin(), output_.end(),' ');
+    state.ResumeTiming();
+    sqeazy::base64_impl(input_.data(),input_.data()+size_,
+                        (char*)output_.data());
+  }
 
+  auto bytes_processed = int64_t(state.iterations()) *  int64_t(size_)*sizeof(input_.front());
+
+  // std::cout << "bytes processed " << bytes_processed << "\n";
+
+  state.SetBytesProcessed(bytes_processed);
+}
+
+BENCHMARK_REGISTER_F(synthetic_data,BM_single_thread_boost)->Arg({1<<8})->Arg({1<<10})->Arg({1 << 12})->Arg({1 << 16})->Arg({4 << 20});;
+
+BENCHMARK_DEFINE_F(synthetic_data, BM_single_thread_mine)(benchmark::State& state) {
+
+
+  if (state.thread_index == 0) {
+    SetUp(state);
+  }
 
   while (state.KeepRunning()) {
     state.PauseTiming();
-    std::fill(output_.begin(), output_.end(),0);
+    std::fill(output_.begin(), output_.end(),' ');
     state.ResumeTiming();
-
-    local.encode(sinus_.data(),
-               output_.data(),
-               shape_);
+    sqeazy::my_base64_impl(input_.data(),input_.data()+size_,
+                        (char*)output_.data());
   }
 
   state.SetBytesProcessed(int64_t(state.iterations()) *
-                          int64_t(size_)*sizeof(sinus_.front()));
+                          int64_t(size_)*sizeof(input_.front()));
 }
 
-BENCHMARK_REGISTER_F(dynamic_default_fixture, single_thread)->Arg({1<<16})->Arg({256 << 10})->Arg({64 << 20});
+BENCHMARK_REGISTER_F(synthetic_data,BM_single_thread_mine)->Arg({1<<8})->Arg({1<<10})->Arg({1 << 12})->Arg({1 << 16})->Arg({4 << 20});
+
+
 
 // BENCHMARK_DEFINE_F(dynamic_default_fixture, two_threads)(benchmark::State& state) {
 
